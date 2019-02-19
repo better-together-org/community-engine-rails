91f7260b0208d0b8cd7a010153fdf4fe764125ea
Robert Smith <rsmithlal@gmail.com>
2019-02-18 20:10:46 -0500

    require friendly_id before initialized and set rspec as the test framework

--- lib/better_together/core/engine.rb
+++ lib/better_together/core/engine.rb
@@ -2,6 +2,15 @@ module BetterTogether
   module Core
     class Engine < ::Rails::Engine
       isolate_namespace BetterTogether::Core
+
+      config.generators do |g|
+        g.test_framework :rspec
+        g.fixture_replacement :factory_bot, :dir => 'spec/factories'
+      end
+
+      config.before_initialize do
+        require 'friendly_id'
+      end
     end
   end
 end
