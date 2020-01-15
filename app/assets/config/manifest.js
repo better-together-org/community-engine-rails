baa643885d3f9858f64c6969edde583948a96795
Robert Smith <rsmithlal@gmail.com>
2020-01-15 18:50:56 -0500

    updated manifest file

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,2 +1,4 @@
+
+//= link_tree ../images/better_together/community
 //= link_directory ../javascripts/better_together/community .js
 //= link_directory ../stylesheets/better_together/community .css
