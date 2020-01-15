a1e63ba12eb75fd786b93fa36441541dbbd0b0cd
Robert Smith <infrastructure@bettertogethersolutions.com>
2020-01-15 17:40:39 -0500

    Update better_together_id.rb
    fixed hound errors

--- app/concerns/better_together/community/better_together_id.rb
+++ app/concerns/better_together/community/better_together_id.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
 
 module BetterTogether
   module Community
+    # Generates a unique identifier for any class that includes this module
     module BetterTogetherId
       extend ActiveSupport::Concern
 
@@ -14,14 +16,13 @@ module BetterTogether
         private
 
         def generate_bt_id
-          return if self.bt_id.present?
+          return if bt_id.present?
           self.bt_id = loop do
             random_token = SecureRandom.uuid
             break random_token unless self.class.exists?(bt_id: random_token)
           end
         end
       end
-
     end
   end
 end
