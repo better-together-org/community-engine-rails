7236ae3445807695fd9d4710fdc26262498b1613
Robert Smith <rsmithlal@gmail.com>
2020-09-21 21:23:44 -0400

    get barebones UI loading

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,5 +1,2 @@
 
-//= link_tree ../images/better_together
-//= link_directory better_together .js
-// link better_together/application.js
-//= link_directory ../stylesheets/better_together .css
+//= link better_together/application.css
