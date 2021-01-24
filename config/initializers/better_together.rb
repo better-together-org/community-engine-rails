b9e111b1b00de788edeefa122532858f20ba0b7f
Robert Smith <rsmithlal@gmail.com>
2021-01-23 22:41:52 -0330

    set default confirm success url from env APP_HOST

--- config/initializers/better_together.rb
+++ config/initializers/better_together.rb
@@ -1,4 +1,7 @@
 require 'better_together'
 
 BetterTogether.user_class = 'BetterTogether::User'
-BetterTogether.default_user_confirm_success_url = 'http://localhost:3000'
+BetterTogether.default_user_confirm_success_url = ENV.fetch(
+  'APP_HOST',
+  'http://localhost:3000'
+)
