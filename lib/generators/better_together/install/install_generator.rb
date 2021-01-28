c8fe883690852e8a2643605945bd3a2d690ca5a4
Robert Smith <rsmithlal@gmail.com>
2021-01-28 03:23:07 -0330

    Fixed incorrect property in install generator

--- lib/generators/better_together/install/install_generator.rb
+++ lib/generators/better_together/install/install_generator.rb
@@ -16,7 +16,7 @@ module BetterTogether
 require 'better_together'
 
 BetterTogether.user_class = 'BetterTogether::User'
-BetterTogether.default_user_confirm_success_url = ENV.fetch(
+BetterTogether.default_user_confirmation_url = ENV.fetch(
   'APP_HOST',
   'http://localhost:3000'
 )
