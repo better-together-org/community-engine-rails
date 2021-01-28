672793f6a7d3951a4d64e9d64d202901f2838e9d
Robert Smith <rsmithlal@gmail.com>
2021-01-28 03:21:38 -0330

    added missing parenthesis in installer generator

--- lib/generators/better_together/install/install_generator.rb
+++ lib/generators/better_together/install/install_generator.rb
@@ -19,6 +19,7 @@ BetterTogether.user_class = 'BetterTogether::User'
 BetterTogether.default_user_confirm_success_url = ENV.fetch(
   'APP_HOST',
   'http://localhost:3000'
+)
 BetterTogether.default_user_new_password_url = ENV.fetch(
   'APP_HOST',
   'http://localhost:3000'
