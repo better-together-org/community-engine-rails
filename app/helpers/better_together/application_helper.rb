0e4c864b979dae100b83b6e05a7417f6e183e8cc
Robert Smith <rob@bettertogethersolutions.com>
2023-11-06 19:57:20 -0330

    Rework application layout to yield content from view
    Moved community layout features to separate file for now.

--- app/helpers/better_together/application_helper.rb
+++ app/helpers/better_together/application_helper.rb
@@ -1,4 +1,32 @@
 module BetterTogether
   module ApplicationHelper
+    
+    def current_identity
+      return unless user_signed_in?
+      return unless current_user.person
+      # TODO: Modify to support when "Active identity" becomes a feature
+      current_user.person
+    end
+
+    # Can search for named routes directly in the main app, omitting
+    # the "better_together." prefix
+    def method_missing method, *args, &block
+      if better_together_url_helper?(method)
+        better_together.send(method, *args)
+      else
+        super
+      end
+    end
+
+    def respond_to?(method)
+      better_together_url_helper?(method) or super
+    end
+
+    private
+
+    def better_together_url_helper?(method)
+      (method.to_s.end_with?('_path') or method.to_s.end_with?('_url')) and
+        better_together.respond_to?(method)
+    end
   end
 end
