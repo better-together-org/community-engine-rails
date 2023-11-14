ad20539c14401f93d5e7459c78adcec32b5c475f
Robert Smith <rob@bettertogethersolutions.com>
2023-11-14 14:32:45 -0330

    Moved Devise API controllers to api directory

---
+++ app/controllers/better_together/bt/api/auth/sessions_controller.rb
@@ -0,0 +1,23 @@
+module BetterTogether
+  module Bt
+    module Api
+      module Auth
+        class SessionsController < Devise::SessionsController
+          respond_to :json
+
+          protected
+
+          def respond_with(resource, _opts = {})
+            render json: resource
+          end
+
+          def respond_to_on_destroy
+            head :ok
+          end
+
+          def after_sign_in_path_for(resource); end
+        end
+      end
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,37 +1,34 @@
 BetterTogether::Engine.routes.draw do
-  devise_for :api_users,
-    class_name: BetterTogether.user_class.to_s,
-    skip: [:unlocks, :omniauth_callbacks],
-    path: 'bt/api/auth',
-    path_names: {
-      sign_in: 'sign-in',
-      sign_out: 'sign-out',
-      registration: 'sign-up'
-    },
-    defaults: { format: :json }
-
-  devise_for :users,
-    class_name: BetterTogether.user_class.to_s,
-    skip: [:unlocks, :omniauth_callbacks],
-    path: 'bt/users',
-    path_names: {
-      sign_in: 'sign-in',
-      sign_out: 'sign-out',
-      registration: 'sign-up'
-    },
-    defaults: { format: :html }
-
   # bt base path
   scope path: 'bt' do
     get '/' => 'static_pages#home'
 
-    
+    devise_for :users,
+      class_name: BetterTogether.user_class.to_s,
+      module: 'devise',
+      skip: [:unlocks, :omniauth_callbacks],
+      path: 'users',
+      path_names: {
+        sign_in: 'sign-in',
+        sign_out: 'sign-out',
+        registration: 'sign-up'
+      },
+      defaults: { format: :html }
   end
 
   namespace :bt do
     namespace :api, defaults: { format: :json } do
-      namespace :v1 do
+      devise_for :users,
+        class_name: BetterTogether.user_class.to_s,
+        skip: [:unlocks, :omniauth_callbacks],
+        path: 'auth',
+        path_names: {
+          sign_in: 'sign-in',
+          sign_out: 'sign-out',
+          registration: 'sign-up'
+        }
 
+      namespace :v1 do
         jsonapi_resources :communities do
           # jsonapi_relationships
         end
