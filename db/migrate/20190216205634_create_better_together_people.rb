3a3cca78381193d5d6ad8e8a3d4dc6b4056ab434
Robert Smith <rob@bettertogethersolutions.com>
2023-11-14 21:14:08 -0330

    Use new helper methods to create tables

--- db/migrate/20190216205634_create_better_together_people.rb
+++ db/migrate/20190216205634_create_better_together_people.rb
@@ -1,23 +1,8 @@
-class CreateBetterTogetherPeople < ActiveRecord::Migration[5.2]
+class CreateBetterTogetherPeople < ActiveRecord::Migration[7.0]
   def change
-    create_table :better_together_people do |t|
-      t.string :bt_id,
-               limit: 100,
-               index: {
-                 name: 'person_by_bt_id',
-                 unique: true
-               },
-               null: false
-      t.string :name,
-               null: false,
-               limit: 50,
-               index: {
-                name: 'by_name'
-               }
-      t.string :description
-
-      t.integer :lock_version, null: false, default: 0
-      t.timestamps null: false
+    create_bt_table :people do |t|
+      t.bt_emoji_name
+      t.bt_emoji_description
     end
   end
 end
