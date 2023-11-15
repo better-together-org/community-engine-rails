5641df4818b652993df85f63aa89724a97dc1410
Robert Smith <rob@bettertogethersolutions.com>
2023-11-15 14:52:31 -0330

    allow for a host community to be designated

--- app/models/better_together/community.rb
+++ app/models/better_together/community.rb
@@ -17,15 +17,33 @@ module BetterTogether
          _prefix: :privacy
 
     belongs_to :creator,
-              class_name: '::BetterTogether::Person'
+              class_name: '::BetterTogether::Person',
+              optional: true
 
     validates :name,
               presence: true
     validates :description,
               presence: true
 
+    validate :single_host_record
+
     def to_s
       name
+    end
+
+    # Method to set the host attribute to true only if there is no host community
+    def set_as_host
+      return if BetterTogether::Community.where(host: true).exists?
+      self.host = true
+    end
+
+    private
+
+    # Validate that only one COmmunity can be marked as host
+    def single_host_record
+      return unless host && BetterTogether::Community.where.not(bt_id: bt_id).exists?(host: true)
+
+      errors.add(:host, 'can only be set for one community')
     end
   end
 end
--- db/migrate/20200115231812_create_better_together_communities.rb
+++ db/migrate/20200115231812_create_better_together_communities.rb
@@ -5,10 +5,14 @@ class CreateBetterTogetherCommunities < ActiveRecord::Migration[7.0]
       t.bt_emoji_description index: { name: 'by_community_description'}
 
       # Reference to the better_together_people table for the creator
-      t.bt_references :creator, null: false, index: { name: 'by_creator' }, target_table: :better_together_people
+      t.bt_references :creator, target_table: :better_together_people, index: { name: 'by_creator' }, null: true
 
       # Adding privacy column
       t.string :privacy, null: false, default: 'public', limit: 50, index: { name: 'by_community_privacy' }
+
+      # Adding a host boolean column with a unique constraint that only allows one true value
+      t.boolean :host, default: false, null: false
+      t.index :host, unique: true, where: "host IS TRUE AND creator_id IS NULL"
 
       # Standard columns like lock_version and timestamps are added by create_bt_table
     end
