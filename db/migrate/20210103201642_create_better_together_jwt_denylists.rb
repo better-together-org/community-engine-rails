334d27cc59ee4e2252eeea210d0716900a1ac380
Robert Smith <rob@bettertogethersolutions.com>
2023-11-14 21:13:25 -0330

    Use bt_create_table for devise tables

--- db/migrate/20210103201642_create_better_together_jwt_denylists.rb
+++ db/migrate/20210103201642_create_better_together_jwt_denylists.rb
@@ -1,18 +1,14 @@
-class CreateBetterTogetherJwtDenylists < ActiveRecord::Migration[6.0]
+class CreateBetterTogetherJwtDenylists < ActiveRecord::Migration[7.0]
   def change
-    create_table :better_together_jwt_denylists do |t|
-      t.string :bt_id,
-               limit: 100,
-               index: {
-                 name: 'jwt_denylist_by_bt_id',
-                 unique: true
-               },
-               null: false
+    create_bt_table :jwt_denylists, pk_index_prefix: 'jwt_denylist' do |t|
+      # Additional columns specific to JWT Denylist
       t.string :jti
       t.datetime :exp
 
-      t.timestamps
+      # Standard columns like lock_version and timestamps are added by create_bt_table
+
+      # Adding index on jti
+      t.index :jti, name: 'index_better_together_jwt_denylists_on_jti'
     end
-    add_index :better_together_jwt_denylists, :jti
   end
 end
