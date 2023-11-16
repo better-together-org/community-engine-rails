44120c84e5be1f1facd7ec2af1837e569261a3d6
Robert Smith <rob@bettertogethersolutions.com>
2023-11-16 01:13:08 -0330

    Fix polymorphic column definitions for friendly_id_slugs

--- db/migrate/20190224040555_create_friendly_id_slugs.rb
+++ db/migrate/20190224040555_create_friendly_id_slugs.rb
@@ -9,13 +9,14 @@ class CreateFriendlyIdSlugs < MIGRATION_CLASS
   def change
     create_table :friendly_id_slugs do |t|
       t.string   :slug,           :null => false
-      t.bt_references :sluggable, polymorphic: true, null: false, index: { name: 'by_sluggable' }
+      t.uuid :sluggable_id, null: false
+      t.string :sluggable_type, null: false
       t.string   :scope
 
       t.integer :lock_version, null: false, default: 0
       t.timestamps null: false
     end
-    add_index :friendly_id_slugs, [:sluggable_type, :sluggable_id]
+    add_index :friendly_id_slugs, [:sluggable_type, :sluggable_id], name: 'by_sluggable'
     add_index :friendly_id_slugs, [:slug, :sluggable_type], length: { slug: 140, sluggable_type: 50 }
     add_index :friendly_id_slugs, [:slug, :sluggable_type, :scope], length: { slug: 70, sluggable_type: 50, scope: 70 }, unique: true
   end
