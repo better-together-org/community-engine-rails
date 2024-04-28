523b757056cb784c31a526f2d67ad5deb2c7cc4f
Robert Smith <rob@bettertogethersolutions.com>
2024-04-28 01:43:02 -0230

    Swap attribute names

--- app/concerns/better_together/joinable.rb
+++ app/concerns/better_together/joinable.rb
@@ -6,8 +6,8 @@ module BetterTogether
     extend ActiveSupport::Concern
 
     included do
-      class_attribute :joinable_membership_associations
-      self.joinable_membership_associations = []
+      class_attribute :member_role_associations
+      self.member_role_associations = []
 
       def self.joinable(joinable_type:, member_type:, **membership_options) # rubocop:todo Metrics/MethodLength
         membership_class = "BetterTogether::#{member_type.camelize}#{joinable_type.camelize}Membership"
@@ -26,12 +26,12 @@ module BetterTogether
                  source: :member,
                  inverse_of: :"member_#{plural_joinable_type}"
 
-        has_many :"#{member_type}_roles",
+        has_many member_roles_association,
                  through: membership_name,
                  source: :role
         
         # Register the association name for role retrieval
-        joinable_membership_associations << member_roles_association
+        member_role_associations << member_roles_association
       end
     end
   end
