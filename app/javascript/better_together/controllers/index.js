18c8762e6e2fb6a12882ae6d31b96f93a7f450c1
Robert Smith <rob@bettertogethersolutions.com>
2024-06-08 16:42:58 -0230

    Fix: Refactor app js to reduce unnecessary complexity

--- app/javascript/better_together/controllers/index.js
+++ app/javascript/better_together/controllers/index.js
@@ -1,10 +1,24 @@
-// Import and register all your controllers from the importmap under controllers/*
 
-import { application } from "better_together/controllers/application"
+import { Application } from "@hotwired/stimulus"
+import FlashController from "./flash_controller"
+// import ModalController from "./modal_controller"
+import DynamicSelectController from "./dynamic_select_controller"
+import NewPersonCommunityMembershipController from "./new_person_community_membership_controller"
+import PersonCommunityMembershipController from "./person_community_membership_controller"
 
-// Eager load all controllers defined in the import map under controllers/**/*_controller
-// import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
-// eagerLoadControllersFrom("controllers", application)
+const application = Application.start()
+
+// Configure Stimulus development experience
+application.debug = true
+window.Stimulus   = application
+
+console.log('start application')
+
+Stimulus.register('flash', FlashController)
+// Stimulus.register('modal', ModalController)
+Stimulus.register('bt-dynamic-select', DynamicSelectController)
+Stimulus.register('bt-new-person-community-membership', NewPersonCommunityMembershipController)
+Stimulus.register('bt-person-community-membership', PersonCommunityMembershipController)
 
 // Lazy load controllers as they appear in the DOM (remember not to preload controllers in import map!)
 import { lazyLoadControllersFrom } from 'stimulus-loading' // Adjust the path if necessary
