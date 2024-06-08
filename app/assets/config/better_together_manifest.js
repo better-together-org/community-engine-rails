0c8570867ee331e483152385adda3d7c693cb7fc
Robert Smith <rob@bettertogethersolutions.com>
2024-06-08 16:32:46 -0230

    attempt to address issue double-loading js controllers

--- app/assets/config/better_together_manifest.js
+++ app/assets/config/better_together_manifest.js
@@ -1,7 +1,6 @@
 // app/assets/config/better_together_manifest.js
 
 //= link_tree ../../javascript/better_together .js
-// link_tree ../../../vendor/javascript .js
 //= link_tree ../stylesheets/better_together .css
 //= link_tree ../images/better_together
 
