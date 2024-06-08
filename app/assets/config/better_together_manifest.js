c557ea38203f5d89fb64d11ceb339a09dcb50177
Robert Smith <rob@bettertogethersolutions.com>
2024-06-08 17:58:13 -0230

    Include stimulus controllers in manifest

--- app/assets/config/better_together_manifest.js
+++ app/assets/config/better_together_manifest.js
@@ -1,6 +1,7 @@
 // app/assets/config/better_together_manifest.js
 
 //= link_tree ../../javascript/better_together .js
+//= link_tree ../../javascript/better_together/controllers .js
 //= link_tree ../stylesheets/better_together .css
 //= link_tree ../images/better_together
 
