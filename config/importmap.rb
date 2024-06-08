48c0431f9df2369e656a9618bb23fd6203ba16f3
Robert Smith <rob@bettertogethersolutions.com>
2024-06-08 14:28:18 -0230

    Fix app-breaking javascript re: stimulus-loading

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,14 +1,15 @@
 # frozen_string_literal: true
 
 # config/importmap.rb
-# pin_all_from File.expand_path("../app/assets/javascript", __dir__)
+
 pin_all_from File.expand_path('../app/javascript', __dir__)
+# pin_all_from File.expand_path('../vendor/javascript', __dir__)
 pin_all_from 'app/javascript/better_together/controllers', under: 'controllers', to: 'controllers'
 pin_all_from 'app/javascript/better_together/trix-extensions', under: 'trix-extensions', to: 'trix-extensions'
+
 pin '@hotwired/turbo-rails', to: 'turbo.min.js'
 pin '@hotwired/stimulus', to: 'stimulus.min.js'
-pin '@hotwired/stimulus-loading', to: 'stimulus-loading'
-# TODO: Check if this is compiled for prod: https://github.com/hotwired/stimulus-rails/issues/108
+pin 'stimulus-loading', to: 'stimulus-loading.js', preload: true # Ensure correct path and file extension
 
 pin 'bootstrap', to: 'bootstrap.min.js', preload: true
 pin '@popperjs/core', to: 'popper.js', preload: true
