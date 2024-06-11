7103abc587f48a4f5f69f8b0db56549694387f43
Robert Smith <rob@bettertogethersolutions.com>
2024-06-11 21:04:08 -0230

    update github oauth action

--- app/controllers/better_together/omniauth_callbacks_controller.rb
+++ app/controllers/better_together/omniauth_callbacks_controller.rb
@@ -1,22 +1,20 @@
 
 class BetterTogether::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   # See https://github.com/omniauth/omniauth/wiki/FAQ#rails-session-is-clobbered-after-callback-on-developer-strategy
-  skip_before_action :verify_authenticity_token, only: :github
 
   def github
-    # You need to implement the method below in your model (e.g. app/models/user.rb)
-    @user = BetterTogether.user_class.constantize.from_omniauth(request.env["omniauth.auth"])
-
+    @user = User.create_from_provider_data(request.env['omniauth.auth'])
     if @user.persisted?
-      sign_in_and_redirect @user, event: :authentication # this will throw if @user is not activated
-      set_flash_message(:notice, :success, kind: "GitHub") if is_navigational_format?
+      sign_in_and_redirect @user
+      set_flash_message(:notice, :success, kind: 'Github') if is_navigational_format?
     else
-      session["devise.github_data"] = request.env["omniauth.auth"].except(:extra) # Removing extra as it can overflow some session stores
+      flash[:error] = 'There was a problem signing you in through Github. Please register or try signing in later.'
       redirect_to new_user_registration_url
     end
   end
 
   def failure
+    flash[:error] = 'There was a problem signing you in. Please register or try signing in later.'
     redirect_to helpers.base_url
   end
 end
