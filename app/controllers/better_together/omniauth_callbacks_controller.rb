1ec7f73d6bf0c45414cd2f1cb8c655c525d1af92
Robert Smith <rob@bettertogethersolutions.com>
2024-06-12 18:26:15 -0230

    rubocop fixes

--- app/controllers/better_together/omniauth_callbacks_controller.rb
+++ app/controllers/better_together/omniauth_callbacks_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module BetterTogether
-  class OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  class OmniauthCallbacksController < Devise::OmniauthCallbacksController # rubocop:todo Style/Documentation
     # See https://github.com/omniauth/omniauth/wiki/FAQ#rails-session-is-clobbered-after-callback-on-developer-strategy
     skip_before_action :verify_authenticity_token, only: %i[github]
 
@@ -11,17 +11,18 @@ module BetterTogether
     attr_reader :person_platform_integration, :user
 
     def github
-      handle_auth "Github"
+      handle_auth 'Github'
     end
 
     private
 
     def handle_auth(kind)
       if user_signed_in?
-        flash[:success] = t 'devise_omniauth_callbacks.success', kind: if is_navigational_format?
+        flash[:success] = t 'devise_omniauth_callbacks.success', kind: kind if is_navigational_format?
         redirect_to edit_user_registration_path
       else
-        flash[:alert] = t 'devise_omniauth_callbacks.failure', kind:, reason: "#{auth.info.email} is not authorized"
+        flash[:alert] =
+          t 'devise_omniauth_callbacks.failure', kind:, reason: "#{auth.info.email} is not authorized"
         redirect_to new_user_registration_path
       end
     end
