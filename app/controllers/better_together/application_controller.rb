7baf6e2af8fcd1129001bd97f2f54443dd478dbd
Robert Smith <rob@bettertogethersolutions.com>
2024-06-18 19:07:08 -0230

    include locale in url by default

--- app/controllers/better_together/application_controller.rb
+++ app/controllers/better_together/application_controller.rb
@@ -55,6 +55,10 @@ module BetterTogether
       end
     end
 
+    def default_url_options
+      { locale: I18n.locale }
+    end
+
     def set_locale
       I18n.locale = params[:locale] || I18n.default_locale
     end
