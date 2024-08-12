93ea5754c529c2e4f3c9ea3cb09823f30c660449
Robert Smith <rob@bettertogethersolutions.com>
2024-08-12 12:15:02 -0230

    Ensure english locale used for navigation seed data

--- app/builders/better_together/navigation_builder.rb
+++ app/builders/better_together/navigation_builder.rb
@@ -7,255 +7,266 @@ module BetterTogether
   class NavigationBuilder < Builder # rubocop:todo Metrics/ClassLength
     class << self
       def seed_data
-        build_header
-        build_header_admin
-        build_better_together
-        build_footer
+        I18n.with_locale(:en) do
+          build_header
+          build_header_admin
+          build_better_together
+          build_footer
 
-        create_unassociated_pages
+          create_unassociated_pages
+        end
       end
 
       def build_better_together # rubocop:todo Metrics/MethodLength
-        # Create Better Together Nav Area
-        better_together_pages = ::BetterTogether::Page.create!(
-          [
-            {
-              title: 'What is Better Together?',
-              slug: 'better-together',
-              published_at: DateTime.current,
-              privacy: 'public',
-              published: true,
-              protected: true,
-              template: 'better_together/static_pages/better_together',
-              content: ''
-            },
-            {
-              title: 'About the Community Engine',
-              slug: 'better-together/community-engine',
-              published_at: DateTime.current,
-              privacy: 'public',
-              published: true,
-              protected: true,
-              template: 'better_together/static_pages/community_engine',
-              content: ''
-            }
-          ]
-        )
-
-        ::BetterTogether::NavigationArea.create! do |area|
-          area.name = 'Better Together'
-          area.slug = 'better-together'
-          area.visible = true
-          area.protected = true
-
-          # Create Admin Navigation Item
-          better_together_nav_item = area.navigation_items.build(
-            title: 'Powered with <3 by Better Together',
-            slug: 'better-together-nav',
-            position: 0,
-            visible: true,
-            protected: true,
-            item_type: 'dropdown',
-            url: '#'
+        I18n.with_locale(:en) do
+          # Create Better Together Nav Area
+          better_together_pages = ::BetterTogether::Page.create!(
+            [
+              {
+                title: 'What is Better Together?',
+                slug: 'better-together',
+                published_at: DateTime.current,
+                privacy: 'public',
+                published: true,
+                protected: true,
+                template: 'better_together/static_pages/better_together',
+                content_en: ''
+              },
+              {
+                title: 'About the Community Engine',
+                slug: 'better-together/community-engine',
+                published_at: DateTime.current,
+                privacy: 'public',
+                published: true,
+                protected: true,
+                template: 'better_together/static_pages/community_engine',
+                content_en: ''
+              }
+            ]
           )
 
-          # Add children to Better Together Navigation Item
-          better_together_nav_item.build_children(better_together_pages, area)
+          ::BetterTogether::NavigationArea.create! do |area|
+            area.name = 'Better Together'
+            area.slug = 'better-together'
+            area.visible = true
+            area.protected = true
+
+            # Create Admin Navigation Item
+            better_together_nav_item = area.navigation_items.build(
+              title: 'Powered with <3 by Better Together',
+              slug: 'better-together-nav',
+              position: 0,
+              visible: true,
+              protected: true,
+              item_type: 'dropdown',
+              url: '#'
+            )
+
+            # Add children to Better Together Navigation Item
+            better_together_nav_item.build_children(better_together_pages, area)
+          end
         end
       end
 
       def build_footer # rubocop:todo Metrics/MethodLength
-        # Create Platform Footer Pages
-        footer_pages = ::BetterTogether::Page.create!(
-          [
-            {
-              title: 'FAQ',
-              slug: 'faq',
-              published_at: DateTime.current,
-              privacy: 'public',
-              published: true,
-              protected: true,
-              template: 'better_together/static_pages/faq',
-              content: ''
-            },
-            {
-              title: 'Privacy Policy',
-              slug: 'privacy-policy',
-              published_at: DateTime.current,
-              privacy: 'public',
-              published: true,
-              protected: true,
-              template: 'better_together/static_pages/privacy',
-              content: ''
-            },
-            {
-              title: 'Terms of Service',
-              slug: 'terms-of-service',
-              published_at: DateTime.current,
-              privacy: 'public',
-              published: true,
-              protected: true,
-              template: 'better_together/static_pages/terms_of_service',
-              content: ''
-            },
-            {
-              title: 'Code of Conduct',
-              slug: 'code-of-conduct',
-              published_at: DateTime.current,
-              privacy: 'public',
-              published: true,
-              protected: true,
-              template: 'better_together/static_pages/code_of_conduct',
-              content: ''
-            },
-            {
-              title: 'Accessibility',
-              slug: 'accessibility',
-              published_at: DateTime.current,
-              privacy: 'public',
-              published: true,
-              protected: true,
-              template: 'better_together/static_pages/accessibility',
-              content: ''
-            },
-            {
-              title: 'Contact',
-              slug: 'contact',
-              published_at: DateTime.current,
-              privacy: 'public',
-              published: true,
-              protected: true,
-              content: <<-HTML
-              <h1 class="page-header mb-3">Contact Us</h1>
-              <p>This is a default contact page for your platform. Be sure to write a real one!</p>
-              HTML
-            }
-          ]
-        )
+        I18n.with_locale(:en) do
+          # Create Platform Footer Pages
+          footer_pages = ::BetterTogether::Page.create!(
+            [
+              {
+                title: 'FAQ',
+                slug: 'faq',
+                published_at: DateTime.current,
+                privacy: 'public',
+                published: true,
+                protected: true,
+                template: 'better_together/static_pages/faq',
+                content_en: ''
+              },
+              {
+                title: 'Privacy Policy',
+                slug: 'privacy-policy',
+                published_at: DateTime.current,
+                privacy: 'public',
+                published: true,
+                protected: true,
+                template: 'better_together/static_pages/privacy',
+                content_en: ''
+              },
+              {
+                title: 'Terms of Service',
+                slug: 'terms-of-service',
+                published_at: DateTime.current,
+                privacy: 'public',
+                published: true,
+                protected: true,
+                template: 'better_together/static_pages/terms_of_service',
+                content_en: ''
+              },
+              {
+                title: 'Code of Conduct',
+                slug: 'code-of-conduct',
+                published_at: DateTime.current,
+                privacy: 'public',
+                published: true,
+                protected: true,
+                template: 'better_together/static_pages/code_of_conduct',
+                content_en: ''
+              },
+              {
+                title: 'Accessibility',
+                slug: 'accessibility',
+                published_at: DateTime.current,
+                privacy: 'public',
+                published: true,
+                protected: true,
+                template: 'better_together/static_pages/accessibility',
+                content_en: ''
+              },
+              {
+                title: 'Contact',
+                slug: 'contact',
+                published_at: DateTime.current,
+                privacy: 'public',
+                published: true,
+                protected: true,
+                content_en: <<-HTML
+                <h1 class="page-header mb-3">Contact Us</h1>
+                <p>This is a default contact page for your platform. Be sure to write a real one!</p>
+                HTML
+              }
+            ]
+          )
 
-        # Create Platform Footer Navigation Area and its Navigation Items
-        ::BetterTogether::NavigationArea.create! do |area|
-          area.name = 'Platform Footer'
-          area.slug = 'platform-footer'
-          area.visible = true
-          area.protected = true
+          # Create Platform Footer Navigation Area and its Navigation Items
+          ::BetterTogether::NavigationArea.create! do |area|
+            area.name = 'Platform Footer'
+            area.slug = 'platform-footer'
+            area.visible = true
+            area.protected = true
 
-          area.build_page_navigation_items(footer_pages)
+            area.build_page_navigation_items(footer_pages)
+          end
         end
       end
 
       def build_header # rubocop:todo Metrics/MethodLength
-        # Create platform header pages
-        header_pages = ::BetterTogether::Page.create(
-          [
-            {
-              title: 'About',
-              slug: 'about',
-              published_at: DateTime.current,
-              privacy: 'public',
-              published: true,
-              protected: true,
-              content: <<-HTML
-              <h1 class="page-header mb-3">About</h1>
-              <p>This is a default about page. Be sure to write a real one!</p>
-              HTML
-            }
-          ]
-        )
+        I18n.with_locale(:en) do
+          # Create platform header pages
+          header_pages = ::BetterTogether::Page.create(
+            [
+              {
+                title: 'About',
+                slug: 'about',
+                published_at: DateTime.current,
+                privacy: 'public',
+                published: true,
+                protected: true,
+                content_en: <<-HTML
+                <h1 class="page-header mb-3">About</h1>
+                <p>This is a default about page. Be sure to write a real one!</p>
+                HTML
+              }
+            ]
+          )
 
-        # Create Platform Header Navigation Area
-        ::BetterTogether::NavigationArea.create! do |area|
-          area.name = 'Platform Header'
-          area.slug = 'platform-header'
-          area.visible = true
-          area.protected = true
+          # Create Platform Header Navigation Area
+          ::BetterTogether::NavigationArea.create! do |area|
+            area.name = 'Platform Header'
+            area.slug = 'platform-header'
+            area.visible = true
+            area.protected = true
 
-          area.build_page_navigation_items(header_pages)
+            area.build_page_navigation_items(header_pages)
+          end
         end
       end
 
       # rubocop:todo Metrics/MethodLength
       def build_header_admin # rubocop:todo Metrics/AbcSize, Metrics/MethodLength
-        # Create Platform Header Admin Navigation Area and its Navigation Items
-        ::BetterTogether::NavigationArea.create! do |area| # rubocop:todo Metrics/BlockLength
-          area.name = 'Platform Header Admin'
-          area.slug = 'platform-header-admin'
-          area.visible = true
-          area.protected = true
+        I18n.with_locale(:en) do
+          # Create Platform Header Admin Navigation Area and its Navigation Items
+          ::BetterTogether::NavigationArea.create! do |area| # rubocop:todo Metrics/BlockLength
+            area.name = 'Platform Header Admin'
+            area.slug = 'platform-header-admin'
+            area.visible = true
+            area.protected = true
 
-          # Create Admin Navigation Item
-          admin_nav = area.navigation_items.build(
-            title: 'Host',
-            slug: 'admin-nav',
-            position: 0,
-            visible: true,
-            protected: true,
-            item_type: 'dropdown',
-            url: '#'
-          )
-
-          # Add children to Admin Navigation Item
-          admin_nav_children = [
-            {
-              title: 'Dashboard',
-              slug: 'host-dashboard',
+            # byebug
+            # Create Admin Navigation Item
+            admin_nav = area.navigation_items.build(
+              title: 'Host',
+              slug: 'admin-nav',
               position: 0,
-              item_type: 'link',
-              url: ::BetterTogether::Engine.routes.url_helpers.host_dashboard_path
-            },
-            {
-              title: 'Communities',
-              slug: 'communities',
-              position: 1,
-              item_type: 'link',
-              url: ::BetterTogether::Engine.routes.url_helpers.communities_path
-            },
-            {
-              title: 'Navigation Areas',
-              slug: 'navigation-areas',
-              position: 2,
-              item_type: 'link',
-              url: ::BetterTogether::Engine.routes.url_helpers.navigation_areas_path
-            },
-            {
-              title: 'Pages',
-              slug: 'pages',
-              position: 3,
-              item_type: 'link',
-              url: ::BetterTogether::Engine.routes.url_helpers.pages_path
-            },
-            {
-              title: 'People',
-              slug: 'people',
-              position: 4,
-              item_type: 'link',
-              url: ::BetterTogether::Engine.routes.url_helpers.people_path
-            },
-            {
-              title: 'Platforms',
-              slug: 'platforms',
-              position: 5,
-              item_type: 'link',
-              url: ::BetterTogether::Engine.routes.url_helpers.platforms_path
-            },
-            {
-              title: 'Roles',
-              slug: 'roles',
-              position: 6,
-              item_type: 'link',
-              url: ::BetterTogether::Engine.routes.url_helpers.roles_path
-            },
-            {
-              title: 'Resource Permissions',
-              slug: 'resource_permissions',
-              position: 7,
-              item_type: 'link',
-              url: ::BetterTogether::Engine.routes.url_helpers.resource_permissions_path
-            }
-          ]
+              visible: true,
+              protected: true,
+              item_type: 'dropdown',
+              url: '#'
+            )
 
-          admin_nav_children.each do |child_attrs|
-            admin_nav.children.build(child_attrs.merge(visible: true, protected: true, navigation_area: area))
+            # Add children to Admin Navigation Item
+            admin_nav_children = [
+              {
+                title: 'Dashboard',
+                slug: 'host-dashboard',
+                position: 0,
+                item_type: 'link',
+                url: ::BetterTogether::Engine.routes.url_helpers.host_dashboard_path
+              },
+              {
+                title: 'Communities',
+                slug: 'communities',
+                position: 1,
+                item_type: 'link',
+                url: ::BetterTogether::Engine.routes.url_helpers.communities_path
+              },
+              {
+                title: 'Navigation Areas',
+                slug: 'navigation-areas',
+                position: 2,
+                item_type: 'link',
+                url: ::BetterTogether::Engine.routes.url_helpers.navigation_areas_path
+              },
+              {
+                title: 'Pages',
+                slug: 'pages',
+                position: 3,
+                item_type: 'link',
+                url: ::BetterTogether::Engine.routes.url_helpers.pages_path
+              },
+              {
+                title: 'People',
+                slug: 'people',
+                position: 4,
+                item_type: 'link',
+                url: ::BetterTogether::Engine.routes.url_helpers.people_path
+              },
+              {
+                title: 'Platforms',
+                slug: 'platforms',
+                position: 5,
+                item_type: 'link',
+                url: ::BetterTogether::Engine.routes.url_helpers.platforms_path
+              },
+              {
+                title: 'Roles',
+                slug: 'roles',
+                position: 6,
+                item_type: 'link',
+                url: ::BetterTogether::Engine.routes.url_helpers.roles_path
+              },
+              {
+                title: 'Resource Permissions',
+                slug: 'resource_permissions',
+                position: 7,
+                item_type: 'link',
+                url: ::BetterTogether::Engine.routes.url_helpers.resource_permissions_path
+              }
+            ]
+
+            admin_nav_children.each do |child_attrs|
+              admin_nav.children.build(child_attrs.merge(visible: true, protected: true, navigation_area: area))
+            end
           end
         end
       end
@@ -269,32 +280,34 @@ module BetterTogether
       end
 
       def create_unassociated_pages # rubocop:todo Metrics/MethodLength
-        # Create Pages not associated with a navigation area
-        ::BetterTogether::Page.create!(
-          [
-            {
-              title: 'Home',
-              slug: 'home-page',
-              published_at: DateTime.current,
-              privacy: 'public',
-              published: true,
-              protected: true,
-              template: 'better_together/static_pages/community_engine',
-              layout: 'layouts/better_together/full_width_page',
-              content: ''
-            },
-            {
-              title: 'Subprocessors',
-              slug: 'subprocessors',
-              published_at: DateTime.current,
-              privacy: 'public',
-              published: true,
-              protected: true,
-              template: 'better_together/static_pages/subprocessors',
-              content: ''
-            }
-          ]
-        )
+        I18n.with_locale(:en) do
+          # Create Pages not associated with a navigation area
+          ::BetterTogether::Page.create!(
+            [
+              {
+                title: 'Home',
+                slug: 'home-page',
+                published_at: DateTime.current,
+                privacy: 'public',
+                published: true,
+                protected: true,
+                template: 'better_together/static_pages/community_engine',
+                layout: 'layouts/better_together/full_width_page',
+                content_en: ''
+              },
+              {
+                title: 'Subprocessors',
+                slug: 'subprocessors',
+                published_at: DateTime.current,
+                privacy: 'public',
+                published: true,
+                protected: true,
+                template: 'better_together/static_pages/subprocessors',
+                content_en: ''
+              }
+            ]
+          )
+        end
       end
 
       def delete_pages
