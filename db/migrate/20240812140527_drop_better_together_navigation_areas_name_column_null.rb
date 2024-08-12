aab31a690f7acda7aaf2d31db12f3bf8e96b6349
Robert Smith <rob@bettertogethersolutions.com>
2024-08-12 15:11:58 -0230

    Remove incorrect migration to change column null for BetterTogether::NavigationArea#name

--- db/migrate/20240812140527_drop_better_together_navigation_areas_name_column_null.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-# Drop column null on BetterTogether::NavigationAreas#name
-class DropBetterTogetherNavigationAreasNameColumnNull < ActiveRecord::Migration[7.1]
-  def change
-    change_column_null :better_together_navigation_areas, :name, false
-  end
-end
