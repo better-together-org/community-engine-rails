5f6ef7f8e02903ab87674288fd769ed3e8c28621
Robert Smith <rob@bettertogethersolutions.com>
2024-08-14 12:52:55 -0230

    Improve ActionText sanitizing

--- config/initializers/action_text.rb
+++ config/initializers/action_text.rb
@@ -1,11 +1,16 @@
 # frozen_string_literal: true
 
+# config/initializers/action_text.rb
 Rails.application.config.after_initialize do
-  # Example of safe array modification
-  if ActionText::ContentHelper.allowed_attributes.frozen?
-    ActionText::ContentHelper.allowed_attributes = ActionText::ContentHelper.allowed_attributes.to_a + %w[style href]
-  else
-    ActionText::ContentHelper.allowed_attributes << 'style'
-    ActionText::ContentHelper.allowed_attributes << 'href'
-  end
+  default_allowed_attributes =
+    Rails::HTML5::Sanitizer.safe_list_sanitizer.allowed_attributes.to_set +
+    ActionText::Attachment::ATTRIBUTES.to_set
+  custom_allowed_attributes = Set.new(%w[style href])
+  ActionText::ContentHelper.allowed_attributes =
+    (default_allowed_attributes + custom_allowed_attributes).freeze
+
+  # default_allowed_tags = Rails::HTML5::Sanitizer.safe_list_sanitizer.allowed_tags.to_set +
+  # Set.new([ActionText::Attachment.tag_name, "figure", "figcaption"])
+  # custom_allowed_tags = Set.new(%w[audio video source])
+  # ActionText::ContentHelper.allowed_tags = (default_allowed_tags + custom_allowed_tags).freeze
 end
