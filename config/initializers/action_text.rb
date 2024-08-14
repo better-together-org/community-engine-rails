5408e34066534e8e81fdd31d6d5079f02dd99281
Robert Smith <rob@bettertogethersolutions.com>
2024-08-14 12:12:56 -0230

    Rubocop fixes

--- config/initializers/action_text.rb
+++ config/initializers/action_text.rb
@@ -3,7 +3,7 @@
 Rails.application.config.after_initialize do
   # Example of safe array modification
   if ActionText::ContentHelper.allowed_attributes.frozen?
-    ActionText::ContentHelper.allowed_attributes = ActionText::ContentHelper.allowed_attributes.to_a + ['style', 'href']
+    ActionText::ContentHelper.allowed_attributes = ActionText::ContentHelper.allowed_attributes.to_a + %w[style href]
   else
     ActionText::ContentHelper.allowed_attributes << 'style'
     ActionText::ContentHelper.allowed_attributes << 'href'
