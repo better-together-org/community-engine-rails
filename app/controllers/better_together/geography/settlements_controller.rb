e00b8073579216a7912a2a7a30fcceacc5b5199f
Robert Smith <rob@bettertogethersolutions.com>
2024-08-22 15:12:01 -0230

    Refactor initial settlement index and show views.
    Still missing updated edit and create views for most geography models. These are not yet user-editable.

--- app/controllers/better_together/geography/settlements_controller.rb
+++ app/controllers/better_together/geography/settlements_controller.rb
@@ -4,10 +4,15 @@ module BetterTogether
   module Geography
     class SettlementsController < FriendlyResourceController # rubocop:todo Style/Documentation
       before_action :set_geography_settlement, only: %i[show edit update destroy]
+      before_action :authorize_geography_settlement, only: %i[show edit update destroy]
+      after_action :verify_authorized, except: :index
 
       # GET /geography/settlements
       def index
-        @geography_settlements = resource_collection
+        authorize resource_class
+        @geography_settlements = policy_scope(
+          resource_collection
+        )
       end
 
       # GET /geography/settlements/1
@@ -16,6 +21,7 @@ module BetterTogether
       # GET /geography/settlements/new
       def new
         @geography_settlement = resource_class.new
+        authorize_geography_settlement
       end
 
       # GET /geography/settlements/1/edit
@@ -24,6 +30,7 @@ module BetterTogether
       # POST /geography/settlements
       def create
         @geography_settlement = resource_class.new(geography_settlement_params)
+        authorize_geography_settlement
 
         if @geography_settlement.save
           redirect_to @geography_settlement, notice: 'Settlement was successfully created.'
@@ -54,9 +61,9 @@ module BetterTogether
         @geography_settlement = set_resource_instance
       end
 
-      # Only allow a list of trusted parameters through.
-      def geography_settlement_params
-        params.fetch(:geography_settlement, {})
+      # Adds a policy check for the settlement
+      def authorize_geography_settlement
+        authorize @geography_settlement
       end
 
       def resource_class
@@ -64,7 +71,15 @@ module BetterTogether
       end
 
       def resource_collection
-        resource_class.all
+        resource_class
+          .includes(:country, :regions, :state)
+          .order(:identifier)
+          .with_translations
+      end
+
+      # Only allow a list of trusted parameters through.
+      def geography_settlement_params
+        params.fetch(:geography_settlement, {})
       end
     end
   end
