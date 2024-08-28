d51738b89e6a62c3a0f24ed8aaf1ec1ac231890b
Robert Smith <rob@bettertogethersolutions.com>
2024-08-28 12:47:07 -0230

    Add premailer-rails to auto-generate mailer text versions

--- lib/better_together/engine.rb
+++ lib/better_together/engine.rb
@@ -14,6 +14,7 @@ require 'devise/jwt'
 require 'font-awesome-sass'
 require 'i18n-timezones'
 require 'importmap-rails'
+require 'premailer/rails'
 require 'reform/rails'
 require 'sprockets/railtie'
 require 'stimulus-rails'
@@ -56,6 +57,19 @@ module BetterTogether
 
     config.time_zone = ENV.fetch('APP_TIME_ZONE', 'Newfoundland')
 
+    initializer "better_together.configure_active_job" do |app|
+      app.config.active_job.queue_adapter = :sidekiq
+    end
+
+    initializer 'better_together.action_mailer' do |app|
+      if Rails.env.development?
+        app.config.action_mailer.show_previews = true
+        app.config.action_mailer.preview_paths = app.config.action_mailer.preview_paths + [ BetterTogether::Engine.root.join('spec/mailers/previews') ]
+      else
+        app.config.action_mailer.show_previews = false
+      end
+    end
+
     # Add engine manifest to precompile assets in production
     initializer 'better_together.assets' do |app|
       # Ensure we are not modifying frozen arrays
@@ -64,10 +78,10 @@ module BetterTogether
                                  root.join('app', 'javascript')] + app.config.assets.paths.to_a
     end
 
-    initializer 'better_together.i18n' do
-      config.i18n.available_locales = ENV.fetch('APP_AVAILABLE_LOCALES', 'en,fr,es').split(',').map(&:to_sym)
-      config.i18n.default_locale = ENV.fetch('APP_DEFAULT_LOCALE', :en).to_sym
-      config.i18n.fallbacks = ENV.fetch('APP_FALLBACK_LOCALES', 'en,fr,es').split(',').map(&:to_sym)
+    initializer 'better_together.i18n' do |app|
+      app.config.i18n.available_locales = ENV.fetch('APP_AVAILABLE_LOCALES', 'en,fr,es').split(',').map(&:to_sym)
+      app.config.i18n.default_locale = ENV.fetch('APP_DEFAULT_LOCALE', :en).to_sym
+      app.config.i18n.fallbacks = ENV.fetch('APP_FALLBACK_LOCALES', 'en,fr,es').split(',').map(&:to_sym)
     end
 
     initializer 'better_together.importmap', before: 'importmap' do |app|
