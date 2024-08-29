75dea6e14c9e787fa99b97cc9f05be84e6ce2581
Robert Smith <rob@bettertogethersolutions.com>
2024-08-29 18:22:41 -0230

    WIP: Allow users to register new user accounts
    Create their associated person record and a community membership for the host community.
    
    Unsure yet about what to do about platform memberships.

--- app/controllers/better_together/users/registrations_controller.rb
+++ app/controllers/better_together/users/registrations_controller.rb
@@ -4,6 +4,29 @@ module BetterTogether
   module Users
     class RegistrationsController < ::Devise::RegistrationsController
       include DeviseLocales
+
+      def create
+        ActiveRecord::Base.transaction do
+          super do |user|
+            return unless user.persisted?
+
+            user.build_person(person_params)
+
+            if user.save!
+              helpers.host_community.person_community_memberships.create!(
+                member: user.person,
+                role: ::BetterTogether::Role.find_by(identifier: 'community_member')
+              )
+            end
+          end
+        end
+      end
+
+      protected
+
+      def person_params
+        params.require(:user).require(:person_attributes).permit(%i[identifier name description])
+      end
     end
   end
 end
