b09b42dc6bf0c55c04acd159e8f4634de59f5658
Robert Smith <rob@bettertogethersolutions.com>
2024-08-30 18:14:15 -0230

    Refactor people and community show views to use tabbed section styling consisten with platform show view
    Use bootstrap card for tabbed section and render community membership details for member and joinable via partials.

--- app/policies/better_together/person_community_membership_policy.rb
+++ app/policies/better_together/person_community_membership_policy.rb
@@ -3,11 +3,15 @@
 module BetterTogether
   class PersonCommunityMembershipPolicy < ApplicationPolicy # rubocop:todo Style/Documentation
     def create?
-      user.present?
+      user.present? && permitted_to?('update_community')
+    end
+
+    def edit?
+      user.present? && permitted_to?('update_community')
     end
 
     def destroy?
-      user.present? && !me? && false
+      user.present? && !me? && permitted_to?('update_community')
     end
 
     class Scope < Scope # rubocop:todo Style/Documentation
