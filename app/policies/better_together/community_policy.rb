3edd9f3bd76c7b4316b851d1701b69bd8e7dd70b
Robert Smith <rob@bettertogethersolutions.com>
2024-08-30 18:19:51 -0230

    Ensure that the base policy scope for communities includes only those that people are authorized to view.
    These includes public communities and communities that a person has either created or is a member of.

--- app/policies/better_together/community_policy.rb
+++ app/policies/better_together/community_policy.rb
@@ -32,7 +32,26 @@ module BetterTogether
 
     class Scope < Scope # rubocop:todo Style/Documentation
       def resolve
-        scope.order(:host, :identifier)
+        scope.order(:host, :identifier).where(permitted_query)
+      end
+
+      protected
+
+      def permitted_query
+        communities_table = ::BetterTogether::Community.arel_table
+        person_community_memberships_table = ::BetterTogether::PersonCommunityMembership.arel_table
+
+        # Only list communities that are public and where the current person is a member or a creator
+        communities_table[:privacy].eq('public').or(
+          communities_table[:id].in(
+            person_community_memberships_table.
+              where(person_community_memberships_table[:member_id].
+              eq(agent.id)).
+              project(:joinable_id)
+          )
+        ).or(
+          communities_table[:creator_id].eq(agent.id)
+        )
       end
     end
   end
