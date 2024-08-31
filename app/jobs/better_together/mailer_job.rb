72ba9749f42df4096f0473e56d73397e5e224655
Robert Smith <rob@bettertogethersolutions.com>
2024-08-31 15:50:32 -0230

    Use correct sidekiq queue for mailer jobs

--- app/jobs/better_together/mailer_job.rb
+++ app/jobs/better_together/mailer_job.rb
@@ -3,6 +3,6 @@
 module BetterTogether
   # Ensures that all mailer jobs use the mailer queue and other common configurations
   class MailerJob < ApplicationJob
-    queue_as :mailer
+    queue_as :mailers
   end
 end
