9751fad33faf196c4cb6cfa63f0e3247ae088ce2
Robert Smith <rob@bettertogethersolutions.com>
2024-08-31 16:05:56 -0230

    Allow accepting invitation during user registration.
    Do not render registration form if platform registration requires an invitation unless a valid invitation_code is present.
    
    Instead, render a form that allows the user to enter an invitation code.

--- app/controllers/better_together/users/registrations_controller.rb
+++ app/controllers/better_together/users/registrations_controller.rb
@@ -6,6 +6,14 @@ module BetterTogether
     class RegistrationsController < ::Devise::RegistrationsController
       include DeviseLocales
 
+      before_action :set_platform_invitation, only: %i[new create]
+
+      def new
+        super do |user|
+          user.email = @platform_invitation.invitee_email if @platform_invitation and user.email.empty?
+        end
+      end
+
       def create # rubocop:todo Metrics/MethodLength
         ActiveRecord::Base.transaction do
           super do |user|
@@ -14,16 +22,38 @@ module BetterTogether
             user.build_person(person_params)
 
             if user.save!
+              if @platform_invitation
+                community_role = @platform_invitation.community_role
+              else
+                community_role = ::BetterTogether::Role.find_by(identifier: 'community_member')
+              end
+               
               helpers.host_community.person_community_memberships.create!(
                 member: user.person,
-                role: ::BetterTogether::Role.find_by(identifier: 'community_member')
+                role: community_role
               )
+
+              if @platform_invitation
+                if @platform_invitation.platform_role
+                  helpers.host_platform.person_platform_memberships.create!(
+                    member: user.person,
+                    role: @platform_invitation.platform_role
+                  )
+                end
+
+                @platform_invitation.accept!(invitee: user.person)
+              end
             end
           end
         end
       end
 
       protected
+
+      def set_platform_invitation
+        return unless params[:invitation_code].present?
+        @platform_invitation = ::BetterTogether::PlatformInvitation.pending.find_by(token: params[:invitation_code])
+      end
 
       def person_params
         params.require(:user).require(:person_attributes).permit(%i[identifier name description])
