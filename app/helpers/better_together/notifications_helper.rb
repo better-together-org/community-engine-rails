612e893496326c6799df415d8dc891ae2a0e25ec
Robert Smith <rob@bettertogethersolutions.com>
2024-09-02 19:47:20 -0230

    Add initial notifications UI and management actions

---
+++ app/helpers/better_together/notifications_helper.rb
@@ -0,0 +1,10 @@
+module BetterTogether
+  module NotificationsHelper
+    def unread_notification_count
+      count = current_person.notifications.unread.size
+      return if count.zero?
+  
+      content_tag(:span, count, class: 'badge bg-primary rounded-pill position-absolute notification-badge')
+    end
+  end
+end
