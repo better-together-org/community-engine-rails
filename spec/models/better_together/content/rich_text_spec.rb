9745cf57116a1942cc0865e5efb511224505643b
Robert Smith <rob@bettertogethersolutions.com>
2024-09-09 00:06:19 -0230

    fix rspec errors

--- spec/models/better_together/content/rich_text_spec.rb
+++ spec/models/better_together/content/rich_text_spec.rb
@@ -3,7 +3,7 @@
 require 'rails_helper'
 
 module BetterTogether
-  RSpec.describe Content::Block::RichText, type: :model do
+  RSpec.describe Content::RichText, type: :model do
     pending "add some examples to (or delete) #{__FILE__}"
   end
 end
