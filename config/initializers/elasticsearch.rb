68782e5cf81191019e16f389c5f61f748a9879f1
Robert Smith <rob@bettertogethersolutions.com>
2024-09-11 16:15:31 -0230

    adjust default elasticsearch host

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -3,6 +3,6 @@
 # config/initializers/elasticsearch.rb
 Elasticsearch::Model.client = Elasticsearch::Client.new(
   port: ENV.fetch('ES_PORT', 9200),
-  host: ENV.fetch('ES_HOST', 'http://es'),
+  host: ENV.fetch('ES_HOST', 'http://elasticsearch'),
   url: ENV.fetch('ELASTICSEARCH_URL', 'http://elasticsearch:9201')
 )
