5b5474b6f2c78372cde1b16272fbdc2838f397f8
Robert Smith <rob@bettertogethersolutions.com>
2024-09-14 10:58:42 -0230

    Add file extension for image asset references

--- app/helpers/better_together/image_helper.rb
+++ app/helpers/better_together/image_helper.rb
@@ -8,7 +8,7 @@ module BetterTogether
       if member.profile_image.attached?
         image_tag(member.profile_image_variant(300).url, class: image_classes, alt: 'Profile Image')
       else
-        image_tag(image_url("default_profile_image_person"), class: image_classes, alt: 'Default Profile Image')
+        image_tag(image_url("default_profile_image_person.jpg"), class: image_classes, alt: 'Default Profile Image')
       end
     end
   end
