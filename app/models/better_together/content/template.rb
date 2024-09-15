dcee46e53f43ecfcee0e1611d72f1448bcafae28
Robert Smith <rob@bettertogethersolutions.com>
2024-09-15 17:13:14 -0230

    Create template block to allow rendering partials as page blocks via template path
    Allows more complex rails-backed content blocks to be rendered in pages

---
+++ app/models/better_together/content/template.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+module BetterTogether
+  module Content
+    # Renders a template from a file
+    class Template < Block
+
+      AVAILABLE_TEMPLATES = %w[
+        better_together/content/blocks/template/default
+      ].freeze
+
+      store_attributes :content_data do
+        template_path String
+      end
+
+      validates :template_path, presence: true, inclusion: { in: ->(template) { template.class::AVAILABLE_TEMPLATES } }
+      
+    end
+  end
+end
