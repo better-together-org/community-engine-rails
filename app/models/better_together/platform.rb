43975e41783558b5f9eda5170d311a4629f07306
Robert Smith <rob@bettertogethersolutions.com>
2024-10-06 19:09:50 -0230

    Add custom css block to platform to allow customizing css sitewide

--- app/models/better_together/platform.rb
+++ app/models/better_together/platform.rb
@@ -33,12 +33,32 @@ module BetterTogether
     has_one_attached :profile_image
     has_one_attached :cover_image
 
+    has_many :platform_blocks, dependent: :destroy, class_name: 'BetterTogether::Content::PlatformBlock'
+    has_many :blocks, through: :platform_blocks
+
     # Virtual attributes to track removal
     attr_accessor :remove_profile_image, :remove_cover_image
 
     # Callbacks to remove images if necessary
     before_save :purge_profile_image, if: -> { remove_profile_image == '1' }
     before_save :purge_cover_image, if: -> { remove_cover_image == '1' }
+
+    def css_block
+      blocks.find_by(type: 'BetterTogether::Content::Css')
+    end
+
+    def css_block?
+      css_block.present?
+    end
+
+    def css_block_attributes= attrs={}
+      block = blocks.find_by(type: 'BetterTogether::Content::Css')
+      if block
+        block.update(attrs.except(:type))
+      else
+        self.platform_blocks.build(block: BetterTogether::Content::Css.new(attrs.except(:type)))
+      end
+    end
 
     def primary_community_extra_attrs
       { host:, protected: }
