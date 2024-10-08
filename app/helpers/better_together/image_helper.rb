601497a7d777c9113ced5fe8038b09f95805a597
Robert Smith <rob@bettertogethersolutions.com>
2024-10-08 01:19:25 -0230

    Improved appearance and accessibility of top nav bar items

--- app/helpers/better_together/image_helper.rb
+++ app/helpers/better_together/image_helper.rb
@@ -4,11 +4,12 @@ module BetterTogether
   module ImageHelper
     def profile_image_tag(member, options = {})
       image_classes =  "profile-image rounded-circle #{options[:class]}"
+      image_style =  "#{options[:style]}"
 
       if member.profile_image.attached?
-        image_tag(member.profile_image_variant(300).url, class: image_classes, alt: 'Profile Image')
+        image_tag(member.profile_image_variant(300).url, class: image_classes, style: image_style, alt: 'Profile Image')
       else
-        image_tag(image_url("default_profile_image_person.jpg"), class: image_classes, alt: 'Default Profile Image')
+        image_tag(image_url("default_profile_image_person.jpg"), class: image_classes, style: image_style, alt: 'Default Profile Image')
       end
     end
   end
