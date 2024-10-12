5f1262adf639b081d9c0a00465fa22ff12f54a23
Robert Smith <rob@bettertogethersolutions.com>
2024-10-12 17:02:52 -0230

    Ensure consistent rendering of profile images across views and partials for models that have profile images
    TODO: Make sure to add default profile images from a host app into this repositiory

--- app/helpers/better_together/image_helper.rb
+++ app/helpers/better_together/image_helper.rb
@@ -1,15 +1,31 @@
-
 module BetterTogether
-  # helps with rendering images
+  # Helps with rendering images for various entities
   module ImageHelper
-    def profile_image_tag(member, options = {})
-      image_classes =  "profile-image rounded-circle #{options[:class]}"
-      image_style =  "#{options[:style]}"
+    def profile_image_tag(entity, options = {})
+      image_classes = "profile-image rounded-circle #{options[:class]}"
+      image_style = "#{options[:style]}"
+      image_size = options[:size] || 300
 
-      if member.profile_image.attached?
-        image_tag(member.profile_image_variant(300).url, class: image_classes, style: image_style, alt: 'Profile Image')
+      # Determine if entity has a profile image
+      if entity.respond_to?(:profile_image) && entity.profile_image.attached?
+        image_tag(entity.profile_image_variant(image_size).url, class: image_classes, style: image_style, alt: 'Profile Image')
       else
-        image_tag(image_url("default_profile_image_person.jpg"), class: image_classes, style: image_style, alt: 'Default Profile Image')
+        # Use a default image based on the entity type
+        default_image = default_profile_image(entity)
+        image_tag(image_url(default_image), class: image_classes, style: image_style, alt: 'Default Profile Image')
+      end
+    end
+
+    private
+
+    def default_profile_image(entity)
+      case entity.class.name
+      when 'BetterTogether::Person'
+        'profile_images/default_profile_image_person.jpg'
+      when 'BetterTogether::Community'
+        'profile_images/default_profile_image_community.jpg'
+      else
+        'profile_images/default_profile_image_generic.jpg'
       end
     end
   end
