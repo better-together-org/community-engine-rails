1470550b55af7c7664c4b5a2069c6326573d6bd0
Robert Smith <rob@bettertogethersolutions.com>
2024-11-02 07:59:53 -0230

    disable external link sanitizer for now. It's marking all links as external.

--- config/initializers/action_text.rb
+++ config/initializers/action_text.rb
@@ -7,26 +7,26 @@ Rails.application.config.to_prepare do
     Set.new(%w[style href])).freeze
 
   # Customize the sanitizer to append icons to external links
-  ActionText::ContentHelper.sanitizer = Rails::HTML5::SafeListSanitizer.new.tap do |sanitizer|
-    sanitizer.extend(Module.new do
-      def sanitize(html, options = {})
-        doc = Nokogiri::HTML::DocumentFragment.parse(html)
-        host = Rails.application.routes.default_url_options[:host] || 'localhost'
+  # ActionText::ContentHelper.sanitizer = Rails::HTML5::SafeListSanitizer.new.tap do |sanitizer|
+  #   sanitizer.extend(Module.new do
+  #     def sanitize(html, options = {})
+  #       doc = Nokogiri::HTML::DocumentFragment.parse(html)
+  #       host = Rails.application.routes.default_url_options[:host] || 'localhost'
 
-        # Process each link to add an icon if it's external
-        doc.css('a[href]').each do |link|
-          link_host = URI.parse(link['href']).host rescue nil
+  #       # Process each link to add an icon if it's external
+  #       doc.css('a[href]').each do |link|
+  #         link_host = URI.parse(link['href']).host rescue nil
 
-          # Append Font Awesome icon if link is external
-          if link_host && link_host != host
-            icon = ActionController::Base.helpers.content_tag(:i, '', class: 'fas fa-external-link-alt ms-1', style: 'font-size: small;')
-            link.add_child(" #{icon}") # Append icon to link text
-            link['class'] = [link['class'], 'external-link'].compact.join(' ') # Add custom class
-          end
-        end
+  #         # Append Font Awesome icon if link is external
+  #         if link_host && link_host != host
+  #           icon = ActionController::Base.helpers.content_tag(:i, '', class: 'fas fa-external-link-alt ms-1', style: 'font-size: small;')
+  #           link.add_child(" #{icon}") # Append icon to link text
+  #           link['class'] = [link['class'], 'external-link'].compact.join(' ') # Add custom class
+  #         end
+  #       end
 
-        super(doc.to_html, options) # Apply the default sanitization
-      end
-    end)
-  end
+  #       super(doc.to_html, options) # Apply the default sanitization
+  #     end
+  #   end)
+  # end
 end
