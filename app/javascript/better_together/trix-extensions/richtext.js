fcff6417e6df834c07e677f30156b23173a0deac
Robert Smith <rob@bettertogethersolutions.com>
2024-11-18 22:11:59 -0330

    limit trix uploads to jpg and png files

--- app/javascript/better_together/trix-extensions/richtext.js
+++ app/javascript/better_together/trix-extensions/richtext.js
@@ -13,6 +13,14 @@ function initializeRichText() {
     addHeadingAttributes()
     addForegroundColorAttributes()
     addBackgroundColorAttributes()
+
+    window.addEventListener("trix-file-accept", function(event) {
+      const acceptedTypes = ['image/jpeg', 'image/png']
+      if (!acceptedTypes.includes(event.file.type)) {
+        event.preventDefault()
+        alert("Only support attachment of jpeg or png files")
+      }
+    })
     window.headingAttributesAdded = true; // Flag to prevent re-adding
   }
 
