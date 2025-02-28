275fd16730d74e11a92751d20bef4be53a0d0b01
Robert Smith <rob@bettertogethersolutions.com>
2025-02-28 18:41:13 -0330

    Re-implement Post model

---
+++ app/models/better_together/post.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+module BetterTogether
+  # Represents a blog post
+  class Post < ApplicationRecord
+    PRIVACY_LEVELS = {
+      private: 'private',
+      public: 'public'
+    }.freeze
+
+    include AuthorableConcern
+    include FriendlySlug
+    include Categorizable
+    include Identifier
+    include Privacy
+    include Publishable
+    include Searchable
+
+    slugged :title
+
+    translates :title
+    translates :content, type: :text
+    translates :content_html, type: :action_text
+
+    enum post_privacy: PRIVACY_LEVELS,
+         _prefix: :post_privacy
+
+    validates :title,
+              presence: true
+
+    validates :content,
+              presence: true
+
+    settings index: { number_of_shards: 1 } do
+      mappings dynamic: 'false' do
+        indexes :title, as: 'title'
+        indexes :content, as: 'content'
+      end
+    end
+
+    def to_s
+      title
+    end
+  end
+end
