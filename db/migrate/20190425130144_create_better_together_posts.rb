dedf6be261ec2d5f3139012abf709111c68ab63b
Robert Smith <rob@bettertogethersolutions.com>
2025-02-28 18:41:13 -0330

    Re-implement Post model

---
+++ db/migrate/20190425130144_create_better_together_posts.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+# Creates posts table
+class CreateBetterTogetherPosts < ActiveRecord::Migration[7.0]
+  def change # rubocop:todo Metrics/MethodLength
+    create_bt_table :posts do |t|
+      t.string "type", default: "BetterTogether::Post", null: false
+      t.bt_identifier
+      t.bt_protected
+      t.bt_privacy
+      t.bt_slug
+
+      t.datetime :published_at,
+                 index: {
+                   name: 'by_post_publication_date'
+                 }
+
+    end
+  end
+end
