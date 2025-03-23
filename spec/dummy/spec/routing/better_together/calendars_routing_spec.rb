e96125d749f9254641c7a062d6d37898189b41b4
Robert Smith <rob@bettertogethersolutions.com>
2025-03-23 19:07:51 -0230

    WIP: Calendar specs

---
+++ spec/dummy/spec/routing/better_together/calendars_routing_spec.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe BetterTogether::CalendarsController, type: :routing do
+  describe 'routing' do
+    it 'routes to #index' do
+      expect(get: '/better_together/calendars').to route_to('better_together/calendars#index')
+    end
+
+    it 'routes to #new' do
+      expect(get: '/better_together/calendars/new').to route_to('better_together/calendars#new')
+    end
+
+    it 'routes to #show' do
+      expect(get: '/better_together/calendars/1').to route_to('better_together/calendars#show', id: '1')
+    end
+
+    it 'routes to #edit' do
+      expect(get: '/better_together/calendars/1/edit').to route_to('better_together/calendars#edit', id: '1')
+    end
+
+    it 'routes to #create' do
+      expect(post: '/better_together/calendars').to route_to('better_together/calendars#create')
+    end
+
+    it 'routes to #update via PUT' do
+      expect(put: '/better_together/calendars/1').to route_to('better_together/calendars#update', id: '1')
+    end
+
+    it 'routes to #update via PATCH' do
+      expect(patch: '/better_together/calendars/1').to route_to('better_together/calendars#update', id: '1')
+    end
+
+    it 'routes to #destroy' do
+      expect(delete: '/better_together/calendars/1').to route_to('better_together/calendars#destroy', id: '1')
+    end
+  end
+end
---
+++ spec/dummy/spec/views/better_together/calendars/new.html.erb_spec.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'better_together/calendars/new', type: :view do
+  before(:each) do
+    assign(:better_together_calendar, BetterTogether::Calendar.new(
+                                        identifier: 'MyString',
+                                        name: 'MyString',
+                                        description: 'MyText',
+                                        slug: 'MyString',
+                                        privacy: 'MyString',
+                                        protected: false
+                                      ))
+  end
+
+  it 'renders new better_together_calendar form' do
+    render
+
+    assert_select 'form[action=?][method=?]', better_together_calendars_path, 'post' do
+      assert_select 'input[name=?]', 'better_together_calendar[identifier]'
+
+      assert_select 'input[name=?]', 'better_together_calendar[name]'
+
+      assert_select 'textarea[name=?]', 'better_together_calendar[description]'
+
+      assert_select 'input[name=?]', 'better_together_calendar[slug]'
+
+      assert_select 'input[name=?]', 'better_together_calendar[privacy]'
+
+      assert_select 'input[name=?]', 'better_together_calendar[protected]'
+    end
+  end
+end
