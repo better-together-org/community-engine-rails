c427ea68cc87fb0f1c5c7691b8666bc2930e5c27
Robert Smith <rob@bettertogethersolutions.com>
2025-03-27 22:43:43 -0230

    Removed db default for map center

---
+++ db/migrate/20250328002116_remove_center_column_default_on_better_together_geography_maps.rb
@@ -0,0 +1,6 @@
+# Removes column default for geography column that's breaking rubocop. Not needed anyway (set in model)
+class RemoveCenterColumnDefaultOnBetterTogetherGeographyMaps < ActiveRecord::Migration[7.1]
+  def change
+    change_column_default(:better_together_geography_maps, :center, nil)
+  end
+end
