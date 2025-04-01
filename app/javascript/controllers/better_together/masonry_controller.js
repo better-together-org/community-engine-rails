e33dec7771ca0f5949889cfad723d8a1cd47dee4
Robert Smith <rob@bettertogethersolutions.com>
2025-04-01 07:51:58 -0230

    Improve image gallery layout

--- app/javascript/controllers/better_together/masonry_controller.js
+++ app/javascript/controllers/better_together/masonry_controller.js
@@ -1,19 +1,18 @@
 import { Controller } from "@hotwired/stimulus";
 import "masonry";
-// import "imagesloaded";
+import "imagesloaded";
 
 // filepath: app/javascript/controllers/better_together/tabs_controller.js
 
 export default class extends Controller {
 
   connect() {
-    this.masonry = new Masonry(this.element, {
-      itemSelector: '.col',
-      percentPosition: true,
-      fitWidth: true
-    })
-    // this.imagesLoaded = imagesLoaded(this.element).progress(function() {
-    //   this.masonry('layout')
-    // })
+    imagesLoaded(this.element, () => {
+      this.masonry = new Masonry(this.element, {
+        itemSelector: '.col',
+        percentPosition: true,
+        fitWidth: true,
+      });
+    });
   }
 }
