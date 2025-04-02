acc4651ac1880694e138c09d984d4b287c884c34
Robert Smith <rob@bettertogethersolutions.com>
2025-04-01 22:21:50 -0230

    Add default to_s method for application_record
    Ensures that all models have a reasonable default according to their relevant string attributes

--- app/models/better_together/application_record.rb
+++ app/models/better_together/application_record.rb
@@ -22,5 +22,14 @@ module BetterTogether
     def cache_key
       "#{I18n.locale}/#{super}"
     end
+
+    def to_s
+      return name if respond_to? :name
+      return title if respond_to? :title
+      return identifier if respond_to? :identifier
+      return slug if respond_to? :slug
+
+      super
+    end
   end
 end
