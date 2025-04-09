7e2e1286918fe1ada69f48c35fd4f18134c1171f
Robert Smith <rob@bettertogethersolutions.com>
2025-04-08 23:12:01 -0230

    Rename BetterTogether::File to BetterTogether::Upload to resolve class name conflict with ::File classs

---
+++ db/migrate/20250409012010_rename_better_together_files_to_better_together_uploads.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class RenameBetterTogetherFilesToBetterTogetherUploads < ActiveRecord::Migration[7.1]
+  def change
+    rename_table :better_together_files, :better_together_uploads, if_exists: true
+    change_column_default :better_together_uploads, :type, from: 'BetterTogether::File', to: 'BetterTogether::Upload'
+    execute <<~SQL
+      UPDATE better_together_uploads
+      SET type = 'BetterTogether::Upload'
+      WHERE type = 'BetterTogether::File'
+    SQL
+  end
+end
