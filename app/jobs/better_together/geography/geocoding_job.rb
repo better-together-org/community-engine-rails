b38d2bf062c5f8593f8e5cbe6393065abbb4b430
Robert Smith <rob@bettertogethersolutions.com>
2025-05-09 15:11:37 -0230

    Add retry rules for geocoder to decrease chance of exceeding the API call limit

--- app/jobs/better_together/geography/geocoding_job.rb
+++ app/jobs/better_together/geography/geocoding_job.rb
@@ -4,10 +4,11 @@ module BetterTogether
   module Geography
     class GeocodingJob < ApplicationJob # rubocop:todo Style/Documentation
       queue_as :geocoding
+      retry_on StandardError, wait: :polynomially_longer, attempts: 5
 
       def perform(geocodable)
-        geocodable.geocode
-        geocodable.save
+        coords = geocodable.geocode
+        geocodable.save if coords
       end
     end
   end
