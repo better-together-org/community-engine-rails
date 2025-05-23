7bed2e4efe0cebca5290fee1532e3e3d031190f9
Robert Smith <rob@bettertogethersolutions.com>
2025-05-23 13:16:15 -0230

    Improve message and conversation styling

--- app/policies/better_together/conversation_policy.rb
+++ app/policies/better_together/conversation_policy.rb
@@ -1,7 +1,20 @@
 # frozen_string_literal: true
 
 module BetterTogether
+  # Access control for conversations
   class ConversationPolicy < ApplicationPolicy
+    def index?
+      user.present?
+    end
+
+    def create?
+      user.present?
+    end
+
+    def show?
+      user.present? && record.participants.include?(agent)
+    end
+
     class Scope < ApplicationPolicy::Scope
     end
   end
