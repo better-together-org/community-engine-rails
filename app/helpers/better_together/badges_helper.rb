3f446b2de0d4519df5dad015164d8630bb15b63c
Robert Smith <rob@bettertogethersolutions.com>
2025-05-26 15:51:35 -0230

    Update communities list to show cards instead of a table

---
+++ app/helpers/better_together/badges_helper.rb
@@ -0,0 +1,15 @@
+
+module BetterTogether
+  # Helpers for Badges
+  module BadgesHelper
+    def privacy_badge(entity, rounded: true, style: 'primary')
+      return unless entity.respond_to? :privacy
+
+      badge_label = entity.privacy.humanize.capitalize
+      rounded_class = rounded ? 'rounded-pill': ''
+      style_class = "text-bg-#{style}"
+
+      content_tag :span, badge_label, class: "badge #{rounded_class} #{style_class}"
+    end
+  end
+end
