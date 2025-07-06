48e0004d8b0798b8ab4849e12042ab7edd03b645
Robert Smith <rob@bettertogethersolutions.com>
2025-07-06 15:14:14 -0230

    Update docs

--- app/channels/application_cable/channel.rb
+++ app/channels/application_cable/channel.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 module ApplicationCable
+  # action cable channel from BetterTogether
   class Channel < ::BetterTogether::ApplicationChannel
   end
 end
--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module ApplicationCable
-  # action cable connection
+  # action cable connection from BetterTogether
   class Connection < ::BetterTogether::ApplicationConnection
   end
 end
