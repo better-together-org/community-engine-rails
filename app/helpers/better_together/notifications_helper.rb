a172696473ad352b9023fe405c11333b2d5b539b
Robert Smith <rob@bettertogethersolutions.com>
2025-07-27 12:18:47 -0230

    Improve notifications dropdown behaviour

--- app/helpers/better_together/notifications_helper.rb
+++ app/helpers/better_together/notifications_helper.rb
@@ -3,12 +3,22 @@
 module BetterTogether
   # groups view logic related to notifications
   module NotificationsHelper
-    def unread_notification_count
-      count = current_person.notifications.unread.size
+    def unread_notifications?
+      unread_notification_count > 0
+    end
+
+    def unread_notification_counter
+      count = unread_notification_count
       return if count.zero?
 
       content_tag(:span, count, class: 'badge bg-primary rounded-pill position-absolute notification-badge',
                                 id: 'person_notification_count')
+    end
+
+    def unread_notification_count
+      return unless current_person
+
+      current_person.notifications.unread.size
     end
 
     def recent_notifications
