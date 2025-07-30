538c88d7a741b64dbc5446b3a1e89f3fe70e9349
Robert Smith <rob@bettertogethersolutions.com>
2025-07-30 20:57:01 -0230

    Initial Conversation creation specs passing

--- spec/support/better_together/conversation_helpers.rb
+++ spec/support/better_together/conversation_helpers.rb
@@ -5,11 +5,18 @@ module BetterTogether
     include Rails.application.routes.url_helpers
     include BetterTogether::Engine.routes.url_helpers
 
-    def create_conversation(*participants)
+    def create_conversation(participants)
       visit new_conversation_path(locale: I18n.default_locale)
+
+      slim_select = find('select[name="conversation[participant_ids][]"]+div.form-select')
+
+      slim_select.click
+
       participants.each do |participant|
-        select "#{participant.person.name} - @#{participant.person.identifier}", from: 'conversation[participant_ids][]'
+        find('.ss-content > .ss-list > .ss-option',
+             text: Regexp.new(participant.slug)).click
       end
+
       click_button 'Create Conversation'
     end
   end
