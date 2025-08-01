4135e288c0cbc57e8501cf08dd725e4a02b2ec75
Jordan Knee <j.knee@live.com>
2025-08-01 10:35:22 -0230

    adds tests for conversation mailer header and body content

--- spec/mailers/better_together/conversation_mailer_spec.rb
+++ spec/mailers/better_together/conversation_mailer_spec.rb
@@ -4,18 +4,29 @@ require 'rails_helper'
 
 module BetterTogether
   RSpec.describe ConversationMailer, type: :mailer do
-    # describe 'new_message_notification' do
-    #   let(:mail) { ConversationMailer.new_message_notification }
+    describe 'new_message_notification' do
+      let!(:host_platform) { create(:platform, :host) }
+      let(:sender) { create(:user) }
+      let(:recipient) { create(:user) }
+      let(:conversation) { create(:conversation, creator: sender.person) }
+      let(:message) { create(:message, conversation: conversation, sender: sender.person) }
 
-    #   it 'renders the headers' do
-    #     expect(mail.subject).to eq('New message notification')
-    #     expect(mail.to).to eq(['to@example.org'])
-    #     expect(mail.from).to eq(['from@example.com'])
-    #   end
+      let(:mail) do
+        ConversationMailer.with(message: message, recipient: recipient.person)
+                          .new_message_notification
+      end
 
-    #   it 'renders the body' do
-    #     expect(mail.body.encoded).to match('Hi')
-    #   end
-    # end
+      it 'renders the headers' do
+        expect(mail.subject).to eq("[#{host_platform.name}] New message in conversation \"#{conversation.title}\"")
+        expect(mail.to).to eq([recipient.email])
+        expect(mail.from).to eq(['community@bettertogethersolutions.com'])
+      end
+
+      it 'renders the body' do
+        expect(mail.body.encoded).to have_content("Hello #{recipient.person.name}")
+        expect(mail.body.encoded).to have_content("#{sender.person.name}:")
+        expect(mail.body.encoded).to have_content(message.content.to_plain_text)
+      end
+    end
   end
 end
