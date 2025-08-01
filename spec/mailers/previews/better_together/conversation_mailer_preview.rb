805eaa62044a67f0efa1ea369510dbbc61f7aad1
Jordan Knee <j.knee@live.com>
2025-08-01 10:49:11 -0230

    conversation_mailer_preview now properly previews a message notification

--- spec/mailers/previews/better_together/conversation_mailer_preview.rb
+++ spec/mailers/previews/better_together/conversation_mailer_preview.rb
@@ -2,20 +2,21 @@
 
 module BetterTogether
   # Preview all emails at http://localhost:3000/rails/mailers/better_together/conversation_mailer
+  # Preview all emails at http://localhost:3000/rails/mailers/better_together/conversation_mailer
   class ConversationMailerPreview < ActionMailer::Preview
     include FactoryBot::Syntax::Methods
     include BetterTogether::ApplicationHelper
 
     # Preview this email at http://localhost:3000/rails/mailers/better_together/conversation_mailer/new_message_notification
     def new_message_notification
-      platform = host_platform || create(:platform)
-      sender = create(:user) 
-      recipient = create(:user) 
-      conversation = create(:conversation, creator: sender.person) 
-      message = create(:message, conversation: conversation, sender: sender.person) 
+      host_platform || create(:platform)
+      sender = create(:user)
+      recipient = create(:user)
+      conversation = create(:conversation, creator: sender.person)
+      message = create(:message, conversation: conversation, sender: sender.person)
 
-       ConversationMailer.with(message: message, recipient: recipient.person)
-      .new_message_notification
+      ConversationMailer.with(message: message, recipient: recipient.person)
+                        .new_message_notification
     end
   end
 end
