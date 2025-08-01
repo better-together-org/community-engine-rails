65d12d29961d72db753ea98edda6b198317f44fc
Jordan Knee <j.knee@live.com>
2025-08-01 16:54:26 -0230

    changes people factory to remove identifier creation

--- spec/factories/better_together/people.rb
+++ spec/factories/better_together/people.rb
@@ -7,8 +7,7 @@ module BetterTogether
     factory :better_together_person, class: Person, aliases: %i[person inviter invitee creator author] do
       id { Faker::Internet.uuid }
       name { Faker::Name.name }
-      description { Faker::Lorem.paragraphs(number: 3) }
-      identifier { Faker::Internet.unique.username(specifier: 5..10) }
+      description { Faker::Lorem.paragraph(sentence_count: 3) }
 
       community
     end
