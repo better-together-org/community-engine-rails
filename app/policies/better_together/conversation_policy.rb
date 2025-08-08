7fcdec6b8773dd7d1c6af08dab7db0ae89253878
Robert Smith <rob@bettertogethersolutions.com>
2025-08-08 15:12:09 -0230

    WIP: Add toggle switch to allow peopel to opt-in to receive messages from members

--- app/policies/better_together/conversation_policy.rb
+++ app/policies/better_together/conversation_policy.rb
@@ -15,7 +15,20 @@ module BetterTogether
       user.present? && record.participants.include?(agent)
     end
 
-    class Scope < ApplicationPolicy::Scope
+  class Scope < ApplicationPolicy::Scope
+  end
+
+  # Returns the people that the agent is permitted to message
+  def permitted_participants
+    if permitted_to?('manage_platform')
+      BetterTogether::Person.all
+    else
+      role = BetterTogether::Role.find_by(identifier: 'platform_manager')
+      manager_ids = BetterTogether::PersonPlatformMembership.where(role_id: role.id).pluck(:member_id)
+      BetterTogether::Person.where(id: manager_ids)
+                          .or(BetterTogether::Person.where("preferences @> ?", { receive_messages_from_members: true }.to_json))
+                          .distinct
     end
+  end
   end
 end
