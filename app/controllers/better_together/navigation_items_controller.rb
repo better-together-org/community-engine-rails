8554f339a5e7b43cb91d557238c5347b7a78948f
Robert Smith <rob@bettertogethersolutions.com>
2025-08-11 18:21:50 -0230

    Refactor navigation item updates for Turbo and HTML

--- app/controllers/better_together/navigation_items_controller.rb
+++ app/controllers/better_together/navigation_items_controller.rb
@@ -44,30 +44,13 @@ module BetterTogether
       end
     end
 
-    def update # rubocop:todo Metrics/AbcSize, Metrics/MethodLength
+    def update
       authorize @navigation_item
 
-      respond_to do |format|
-        if @navigation_item.update(navigation_item_params)
-          flash[:notice] = t('navigation_item.updated')
-          format.html { redirect_to @navigation_area, notice: t('navigation_item.updated') }
-          format.turbo_stream do
-            redirect_to @navigation_area, only_path: true
-          end
-        else
-          flash.now[:alert] = t('navigation_item.update_failed')
-          format.html { render :edit, status: :unprocessable_entity }
-          format.turbo_stream do
-            render turbo_stream: [
-              turbo_stream.update('form_errors', partial: 'layouts/better_together/errors',
-                                                 locals: { object: @navigation_item }),
-              turbo_stream.update('navigation_item_form', partial: 'better_together/navigation_items/form',
-                                                          # rubocop:todo Layout/LineLength
-                                                          locals: { navigation_item: @navigation_item, navigation_area: @navigation_area })
-              # rubocop:enable Layout/LineLength
-            ]
-          end
-        end
+      if @navigation_item.update(navigation_item_params)
+        handle_update_success
+      else
+        handle_update_failure
       end
     end
 
@@ -79,6 +62,30 @@ module BetterTogether
     end
 
     private
+
+    def handle_update_success
+      flash[:notice] = t('navigation_item.updated')
+      respond_to do |format|
+        format.html { redirect_to @navigation_area, notice: flash[:notice] }
+        format.turbo_stream { redirect_to @navigation_area, only_path: true }
+      end
+    end
+
+    def handle_update_failure
+      flash.now[:alert] = t('navigation_item.update_failed')
+      respond_to do |format|
+        format.html { render :edit, status: :unprocessable_entity }
+        format.turbo_stream do
+          render turbo_stream: [
+            turbo_stream.update('form_errors', partial: 'layouts/better_together/errors',
+                                               locals: { object: @navigation_item }),
+            turbo_stream.update('navigation_item_form', partial: 'better_together/navigation_items/form',
+                                                        locals: { navigation_item: @navigation_item,
+                                                                  navigation_area: @navigation_area })
+          ]
+        end
+      end
+    end
 
     def available_parent_items
       BetterTogether::NavigationItem.where.not(id: @navigation_item.id).includes(
