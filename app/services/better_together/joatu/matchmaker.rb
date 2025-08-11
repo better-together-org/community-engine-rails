12b259470fbde4f7b1b1e22c474e60944d08ae64
Robert Smith <rob@bettertogethersolutions.com>
2025-08-11 18:50:37 -0230

    Scope Joatu matches to request target

--- app/services/better_together/joatu/matchmaker.rb
+++ app/services/better_together/joatu/matchmaker.rb
@@ -6,6 +6,8 @@ module BetterTogether
     class Matchmaker
       def self.match(request)
         BetterTogether::Joatu::Offer.status_open
+                                    .where(target_type: request.target_type,
+                                           target_id: request.target_id)
                                     .joins(:categories)
                                     .where(BetterTogether::Joatu::Category.table_name => { id: request.category_ids })
                                     .where.not(creator_id: request.creator_id)
