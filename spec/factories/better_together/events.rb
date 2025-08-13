6962b57dd5cd80976139b87df46fb3ff77ebb21c
Robert Smith <rob@bettertogethersolutions.com>
2025-08-13 08:50:08 -0230

    fix event specs and factories

--- spec/factories/better_together/events.rb
+++ spec/factories/better_together/events.rb
@@ -1,6 +1,14 @@
 # frozen_string_literal: true
 
+require 'faker'
+
 FactoryBot.define do
-  factory :event do # rubocop:todo Lint/EmptyBlock
+  factory :event, class: 'BetterTogether::Event' do
+    name { "Event \#{Faker::Lorem.unique.word}" }
+    starts_at { 1.day.from_now }
+
+    trait :draft do
+      after(:create) { |event| event.update_column(:starts_at, nil) }
+    end
   end
 end
