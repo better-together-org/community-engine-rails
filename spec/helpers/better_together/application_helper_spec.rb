ea00b8c858b4457d99891f19015a2da0a0287bf9
Robert Smith <rob@bettertogethersolutions.com>
2025-08-13 08:58:19 -0230

    test: add meta description helper spec

--- spec/helpers/better_together/application_helper_spec.rb
+++ spec/helpers/better_together/application_helper_spec.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
+require 'nokogiri'
 
 RSpec.describe BetterTogether::ApplicationHelper do
   describe '#base_url' do
@@ -49,6 +50,23 @@ RSpec.describe BetterTogether::ApplicationHelper do
         helper.content_for(:canonical_url, 'https://external.test/path')
         result = helper.canonical_link_tag
         expect(result).to include('href="https://external.test/path"')
+      end
+    end
+
+    describe '#set_meta_description' do
+      it 'stores translated description in content_for and renders meta tag' do
+        allow(helper).to receive(:host_platform).and_return(double(name: 'MyPlatform'))
+        allow(helper).to receive(:request).and_return(double(original_url: 'http://example.com'))
+        allow(helper).to receive(:host_community_logo_url).and_return(nil)
+
+        helper.set_meta_description('communities.index', platform_name: 'MyPlatform')
+
+        expected = I18n.t('meta.descriptions.communities.index', platform_name: 'MyPlatform')
+        expect(view.content_for(:meta_description)).to eq(expected)
+
+        html = Nokogiri::HTML.fragment(helper.seo_meta_tags)
+        meta = html.at('meta[name="description"]')
+        expect(meta['content']).to eq(expected)
       end
     end
   end
