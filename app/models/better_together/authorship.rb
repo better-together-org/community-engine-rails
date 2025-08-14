1aff1ca2b17f857f461066cc06ca29b8f0ab03d8
Robert Smith <rob@bettertogethersolutions.com>
2025-08-14 15:37:13 -0230

    Notify authors of new authorship assignments

--- app/models/better_together/authorship.rb
+++ app/models/better_together/authorship.rb
@@ -9,5 +9,37 @@ module BetterTogether
                class_name: 'BetterTogether::Person'
     belongs_to :authorable,
                polymorphic: true
+
+    # Notify authors when they are added to or removed from a Page
+    after_commit :notify_added_to_page, on: :create
+    after_commit :notify_removed_from_page, on: :destroy
+
+    private
+
+    def notify_added_to_page
+      return unless authorable.is_a?(BetterTogether::Page)
+
+      actor = defined?(::Current) && ::Current.respond_to?(:person) ? ::Current.person : nil
+      BetterTogether::PageAuthorshipNotifier
+        .with(record: authorable,
+              page_id: authorable.id,
+              action: 'added',
+              actor_id: actor&.id,
+              actor_name: actor&.name)
+        .deliver_later(author)
+    end
+
+    def notify_removed_from_page
+      return unless authorable.is_a?(BetterTogether::Page)
+
+      actor = defined?(::Current) && ::Current.respond_to?(:person) ? ::Current.person : nil
+      BetterTogether::PageAuthorshipNotifier
+        .with(record: authorable,
+              page_id: authorable.id,
+              action: 'removed',
+              actor_id: actor&.id,
+              actor_name: actor&.name)
+        .deliver_later(author)
+    end
   end
 end
