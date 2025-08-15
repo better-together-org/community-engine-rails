0019e5af89a85858685a555c705aefa4cd560076
Robert Smith <rob@bettertogethersolutions.com>
2025-08-15 09:53:32 -0230

    Re-add missing polymorphic category to categorization

--- app/models/better_together/categorization.rb
+++ app/models/better_together/categorization.rb
@@ -2,7 +2,7 @@
 
 module BetterTogether
   class Categorization < ApplicationRecord # rubocop:todo Style/Documentation
-    belongs_to :category
+    belongs_to :category, polymorphic: true
     belongs_to :categorizable, polymorphic: true, touch: true
   end
 end
