425cc7a6cccd7059b62a09cf333aa526bd432804
Robert Smith <rob@bettertogethersolutions.com>
2025-08-18 20:36:27 -0230

    WIP: Show registration agreements in modal instead of new page

--- app/controllers/better_together/agreements_controller.rb
+++ app/controllers/better_together/agreements_controller.rb
@@ -5,6 +5,17 @@ module BetterTogether
   class AgreementsController < FriendlyResourceController
     skip_before_action :check_platform_privacy, only: :show
 
+    # When the agreement is requested inside a Turbo Frame (from the modal),
+    # return only the fragment wrapped in the expected <turbo-frame id="agreement_modal_frame">...</turbo-frame>
+    # so Turbo can swap it into the frame. For normal requests, fall back to the
+    # default rendering (with layout).
+    def show
+      return unless turbo_frame_request?
+
+      content = render_to_string(action: :show, layout: false)
+      render html: "<turbo-frame id=\"agreement_modal_frame\">#{content}</turbo-frame>".html_safe
+    end
+
     protected
 
     def resource_class
