9651b05f4f779d6e05b1285a1c5a93428d5f902a
Robert Smith <rob@bettertogethersolutions.com>
2025-08-19 13:27:16 -0230

    Rubocop fixes

--- spec/models/better_together/post_spec.rb
+++ spec/models/better_together/post_spec.rb
@@ -2,14 +2,14 @@
 
 require 'rails_helper'
 
-RSpec.describe BetterTogether::Post, type: :model do
+RSpec.describe BetterTogether::Post do
   it_behaves_like 'an authorable model'
 
   it 'has a valid factory' do
     expect(build(:better_together_post)).to be_valid
   end
 
-  it 'validates presence of title and content' do
+  it 'validates presence of title and content' do # rubocop:todo RSpec/MultipleExpectations
     post = build(:better_together_post, title: nil, content: nil)
     expect(post).not_to be_valid
     expect(post.errors[:title]).to include("can't be blank")
@@ -24,7 +24,7 @@ RSpec.describe BetterTogether::Post, type: :model do
   end
 
   describe 'after_create #add_creator_as_author' do
-    it 'creates an authorship for the creator_id' do
+    it 'creates an authorship for the creator_id' do # rubocop:todo RSpec/ExampleLength
       creator = create(:better_together_person)
       post = build(:better_together_post)
       # Ensure no prebuilt authorships from the factory
