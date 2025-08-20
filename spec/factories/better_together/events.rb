40e90bdf133cd98f89d0b95ab7372576e9526797
Jordan Knee <j.knee@live.com>
2025-08-20 15:43:58 -0230

    expands the events factory

--- spec/factories/better_together/events.rb
+++ spec/factories/better_together/events.rb
@@ -1,6 +1,14 @@
 # frozen_string_literal: true
 
-FactoryBot.define do
-  factory :event do # rubocop:todo Lint/EmptyBlock
+module BetterTogether
+  FactoryBot.define do
+    factory :better_together_event, class: Event, aliases: %i[event] do
+      name { Faker::Theater.play }
+      description { Faker::Lorem.paragraphs(number: 3) }
+      starts_at { nil }
+      ends_at { nil }
+
+      association :creator, factory: :person
+    end
   end
 end
