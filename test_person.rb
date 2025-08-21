fb62b9509197f8936d1d7683bc732b9bcd8311a9
Robert Smith <rob@bettertogethersolutions.com>
2025-08-21 18:12:31 -0230

    chore: Remove test_person.rb file as it is no longer needed

--- test_person.rb
+++
@@ -1,6 +0,0 @@
-person = FactoryBot.create(:better_together_person)
-puts "Person email: #{person.email}"
-puts "Email addresses count: #{person.email_addresses.count}"
-person.email_addresses.each { |ea| puts "  - #{ea.email} (primary: #{ea.primary_flag})" }
-puts "Contact detail: #{person.contact_detail}"
-puts "Contact detail email addresses: #{person.contact_detail.email_addresses.count}" if person.contact_detail
