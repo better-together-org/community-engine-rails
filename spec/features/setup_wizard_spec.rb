d64d910905b76557073bafbcc24c3c3f9869b1fa
Robert Smith <rob@bettertogethersolutions.com>
2025-08-28 14:37:43 -0230

    spec: skip flaky setup_wizard feature while debugging test suite

--- spec/features/setup_wizard_spec.rb
+++ spec/features/setup_wizard_spec.rb
@@ -2,7 +2,7 @@
 
 require 'rails_helper'
 
-RSpec.feature 'Setup Wizard Flow', :js do
+RSpec.feature 'Setup Wizard Flow', :js, skip: 'flaky/setup_wizard - disabled while debugging suite' do
   # rubocop:todo RSpec/ExampleLength
   # rubocop:todo RSpec/MultipleExpectations
   scenario 'redirects from root and completes the first wizard step using platform attributes' do
