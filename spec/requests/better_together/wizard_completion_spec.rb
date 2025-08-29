8c47a49ec385f441a7080be63dc0b3d8be7d28c8
Robert Smith <rob@bettertogethersolutions.com>
2025-08-29 17:53:08 -0230

    Rubocop fixes

--- spec/requests/better_together/wizard_completion_spec.rb
+++ spec/requests/better_together/wizard_completion_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe 'Setup Wizard completion' do
     wizard.mark_completed
   end
 
-  it 'redirects to the success path with notice preserved' do
+  it 'redirects to the success path with notice preserved' do # rubocop:disable RSpec/MultipleExpectations
     get better_together.setup_wizard_path(locale: I18n.locale)
     expect(response).to redirect_to(wizard.success_path)
 
