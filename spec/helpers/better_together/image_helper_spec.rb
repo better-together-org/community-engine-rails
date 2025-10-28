882f013b216bb731efd23956ad5df539e84ffb56
Robert Smith <rob@bettertogethersolutions.com>
2025-10-28 20:42:33 -0230

    refactor(image_helper_spec): simplify response stubbing for profile image methods

--- spec/helpers/better_together/image_helper_spec.rb
+++ spec/helpers/better_together/image_helper_spec.rb
@@ -18,26 +18,23 @@ RSpec.describe BetterTogether::ImageHelper do
 
     context 'when person has profile_image_url method' do
       before do
-        allow(person).to receive(:respond_to?).with(:profile_image).and_return(true)
-        allow(person).to receive(:respond_to?).with(:profile_image, anything).and_return(true)
-        allow(person).to receive(:respond_to?).with(:profile_image_url).and_return(true)
-        allow(person).to receive(:respond_to?).with(:profile_image_url, anything).and_return(true)
-
         # Mock profile_image.attached? to return true
         profile_image_double = double('profile_image', attached?: true) # rubocop:todo RSpec/VerifiedDoubles
-        allow(person).to receive_messages(respond_to?: false, profile_image_url: 'http://example.com/optimized.jpg',
-                                          profile_image: profile_image_double)
+        allow(person).to receive(:profile_image).and_return(profile_image_double)
+
+        # Stub the profile_image_url method to return our test URL
+        allow(person).to receive(:profile_image_url).and_return('http://example.com/optimized.jpg')
       end
 
       it 'uses the optimized profile_image_url method' do
-        expect(person).to receive(:profile_image_url).with(size: 300)
+        expect(person).to receive(:profile_image_url).with(size: 300).and_return('http://example.com/optimized.jpg')
         result = profile_image_tag(person)
         expect(result).to include('src="http://example.com/optimized.jpg"')
         expect(result).to include('class="profile-image rounded-circle')
       end
 
       it 'respects custom size parameter' do
-        expect(person).to receive(:profile_image_url).with(size: 150)
+        expect(person).to receive(:profile_image_url).with(size: 150).and_return('http://example.com/optimized.jpg')
         profile_image_tag(person, size: 150)
       end
     end
