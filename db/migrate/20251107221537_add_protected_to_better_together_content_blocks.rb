b6c5a6638732b36ccf34fc20924e9bacc991fd93
Robert Smith <rob@bettertogethersolutions.com>
2025-11-19 13:49:48 -0330

    Adjust migration version number to ensure compatibility with Rails 7.2 maintained

--- db/migrate/20251107221537_add_protected_to_better_together_content_blocks.rb
+++ db/migrate/20251107221537_add_protected_to_better_together_content_blocks.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 # Adds protected column to prevent deletion of platform-critical blocks
-class AddProtectedToBetterTogetherContentBlocks < ActiveRecord::Migration[8.0]
+class AddProtectedToBetterTogetherContentBlocks < ActiveRecord::Migration[7.2]
   def change
     change_table :better_together_content_blocks, bulk: true, &:bt_protected
   end
--- db/migrate/20251107222131_add_creator_to_better_together_platforms.rb
+++ db/migrate/20251107222131_add_creator_to_better_together_platforms.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 # Adds creator association to platforms
-class AddCreatorToBetterTogetherPlatforms < ActiveRecord::Migration[8.0]
+class AddCreatorToBetterTogetherPlatforms < ActiveRecord::Migration[7.2]
   def change
     change_table :better_together_platforms, bulk: true do |t|
       t.bt_creator :better_together_platforms
