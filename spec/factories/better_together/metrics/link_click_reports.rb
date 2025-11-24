053550f3c181f0de0acfba9ad576d4417197aa5e
Robert Smith <rob@bettertogethersolutions.com>
2025-11-24 15:11:55 -0330

    feat: enhance metrics factories and specs for downloads, link clicks, shares, and page views

--- spec/factories/better_together/metrics/link_click_reports.rb
+++ spec/factories/better_together/metrics/link_click_reports.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
-  factory :metrics_link_click_report, class: 'Metrics::LinkClickReport' do # rubocop:todo Lint/EmptyBlock
+  factory :metrics_link_click_report, class: 'BetterTogether::Metrics::LinkClickReport', aliases: [:link_click_report] do
+    file_format { 'csv' }
+    filters { {} }
   end
 end
