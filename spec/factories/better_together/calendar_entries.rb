834d3d6d628b7e11a52bb91a69a1e6641be4cc68
Robert Smith <rob@bettertogethersolutions.com>
2025-11-24 15:14:52 -0330

    feat: add factory and specs for calendar_entry with validations and associations

--- spec/factories/better_together/calendar_entries.rb
+++ spec/factories/better_together/calendar_entries.rb
@@ -1,6 +1,11 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
-  factory :calendar_entry do # rubocop:todo Lint/EmptyBlock
+  factory :calendar_entry, class: 'BetterTogether::CalendarEntry', aliases: [:better_together_calendar_entry] do
+    association :calendar, factory: :calendar
+    association :event, factory: :event
+    starts_at { 1.week.from_now }
+    ends_at { 1.week.from_now + 2.hours }
+    duration_minutes { 120 }
   end
 end
