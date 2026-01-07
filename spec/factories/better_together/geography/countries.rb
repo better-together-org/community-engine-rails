b0e9fb6922c0c2714cbb967d6b9b07ce4965578d
Robert Smith <rob@bettertogethersolutions.com>
2026-01-07 16:06:08 -0330

    feat: enhance geography_country factory name with unique number

--- spec/factories/better_together/geography/countries.rb
+++ spec/factories/better_together/geography/countries.rb
@@ -2,7 +2,7 @@
 
 FactoryBot.define do
   factory :geography_country, class: '::BetterTogether::Geography::Country', aliases: %i[country] do
-    name { Faker::Address.unique.country }
+    name { Faker::Address.unique.country + " #{Faker::Number.unique.number(digits: 5)}" }
     description { Faker::Lorem.paragraphs(number: 3) }
 
     iso_code { Faker::String.random(length: 2).to_s }
