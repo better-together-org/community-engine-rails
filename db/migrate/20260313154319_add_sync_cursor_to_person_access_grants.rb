dd5e4c80f58bea671f972c2c3e31f3f869f2d582
Robert Smith <rob@bettertogethersolutions.com>
2026-03-26 14:23:53 -0230

    fix: make federation migrations idempotent

--- db/migrate/20260313154319_add_sync_cursor_to_person_access_grants.rb
+++ db/migrate/20260313154319_add_sync_cursor_to_person_access_grants.rb
@@ -2,6 +2,7 @@
 
 class AddSyncCursorToPersonAccessGrants < ActiveRecord::Migration[7.2]
   def change
-    add_column :better_together_person_access_grants, :sync_cursor, :string
+    add_column :better_together_person_access_grants, :sync_cursor, :string unless column_exists?(:better_together_person_access_grants,
+                                                                                                  :sync_cursor)
   end
 end
